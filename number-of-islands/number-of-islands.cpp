382898e8fe71ea2f93d67dd63e931ff8a618a265
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2021-12-20 21:14:16 +0530

    Time: 28 ms (96.47%), Space: 12.2 MB (97.37%) - LeetHub

---
+++ number-of-islands/number-of-islands.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool isSafe(int i, int j, int m, int n)
+    {
+        if(i>=0 && i<m && j>=0 && j<n)
+            return true;
+    return false;
+    }
+        
+    void dfs(int i, int j, int m, int n, vector<vector<char>>& grid)
+    {
+        if(!isSafe(i,j,m,n) || grid[i][j]!='1')
+            return;
+        
+        if(grid[i][j]=='1')
+            grid[i][j]='0';
+        
+        dfs(i-1,j,m,n,grid);
+        dfs(i+1,j,m,n,grid);
+        dfs(i,j+1,m,n,grid);
+        dfs(i,j-1,m,n,grid);
+        
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+       int m=grid.size();
+       int n=grid[0].size();
+        int c=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    c++;
+                    dfs(i,j,m,n,grid);
+                }
+                    
+            }
+        }
+        
+    return c;
+    }
+};
