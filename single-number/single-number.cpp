455bf0d98fbd94eb851b8c2c15c3f128bf98f281
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2021-12-29 16:59:13 +0530

    Time: 48 ms (5.10%), Space: 17 MB (47.92%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+          int d=0;
+        for(int i=0;i<nums.size();i++)
+            d=d^nums[i];
+        
+        return d;
+    }
+};
