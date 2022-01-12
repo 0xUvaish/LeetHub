74357a775c7c63d5bf0cdda554f9f2bd68a10864
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-01-12 12:28:54 +0530

    Added solution - LeetHub

--- Min distance between two given nodes of a Binary Tree - GFG/min-distance-between-two-given-nodes-of-a-binary-tree.cpp
+++ Min distance between two given nodes of a Binary Tree - GFG/min-distance-between-two-given-nodes-of-a-binary-tree.cpp
@@ -99,20 +99,24 @@ class Solution{
     in a tree with given root*/
 // firstly find the the LCA node for both
    
-     Node * LCA(Node*root , int n1  , int n2){
-     // base case 
-     if(root==NULL) return NULL ; 
-     if(root->data == n1 || root->data == n2) return root ;
-
-     // create and left and right pointer
-     Node*left = LCA (root->left , n1 , n2);
-     Node*right = LCA (root->right , n1 , n2);
-
-     if(left && right) return root ;
-
-     if(left) return left;
-
-     return right ;
+     Node * LCA(Node*root , int n1  , int n2)
+     {
+         if(root==NULL) 
+            return NULL; 
+         if(root->data == n1 || root->data == n2) 
+            return root;
+    
+         // create and left and right pointer
+         Node*left = LCA(root->left, n1, n2);
+         Node*right = LCA(root->right, n1, n2);
+    
+         if(left && right)
+            return root;
+    
+         if(left)
+            return left;
+    
+         return right;
 
 
  }
