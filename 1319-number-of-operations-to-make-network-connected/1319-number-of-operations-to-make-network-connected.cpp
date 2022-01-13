fab481e15b4313398d41b144a5c4225f798c58a5
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-01-13 14:32:15 +0530

    Time: 241 ms (14.54%), Space: 37.3 MB (98.23%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+        
+   int parent(int a,vector<int>& un)
+    {
+        while(un[a]!=-1)
+            a=un[a];
+       return a;  
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& connections)
+    {
+       int size=connections.size(),ans=0;
+        if(size<n-1)
+            return -1;
+        
+        vector<int>un(n,-1);
+
+        for(int i=0;i<size;i++)
+        {
+           auto a = parent(connections[i][0], un);
+           auto b = parent(connections[i][1], un);
+            
+            if(a!=b)
+                un[b]=a;
+        }
+        for(auto & i:un)
+            if(i==-1)  //nodes which are non reachable
+                ans++;
+        
+        return ans-1;   //because un[0]=-1 for first node
+    }
+
+};
