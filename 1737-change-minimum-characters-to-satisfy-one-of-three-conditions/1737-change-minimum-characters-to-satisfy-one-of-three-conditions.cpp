1f879aa4ef7db6919bb7625cb5fed2a57235783e
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-01-15 22:14:52 +0530

    Time: 44 ms (82.22%), Space: 14.9 MB (20.00%) - LeetHub

--- 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
+++ 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-int minCharacters(string str1, string str2){
-    int len1 = (int) str1.length(), len2 = (int) str2.length();
+int minCharacters(string str1, string str2)
+{
+    int l1 = (int) str1.length(), l2 = (int) str2.length();
     vector<int> freq1(26, 0), freq2(26, 0);
-    int ans = len1 + len2;
-    for(auto x: str1){
+    int ans = l1 + l2;
+    
+    for(auto x: str1)
         freq1[x-'a']++;
-    }
-    for(auto x: str2){
+    
+    for(auto x: str2)
         freq2[x-'a']++;
-    }
-    for(int i = 0; i < 26; i++){
-        ans = min(ans, len1 - freq1[i] + len2 - freq2[i]);
-        if(i > 0){
-            freq1[i] += freq1[i-1], freq2[i] += freq2[i-1];
+    
+    for(int i = 0; i < 26; i++)
+    {
+        ans = min(ans, l1 - freq1[i] + l2 - freq2[i]);
+        
+        if(i > 0)
+        {
+            freq1[i] += freq1[i-1];
+            freq2[i] += freq2[i-1];
         }
-        if(i < 25){
-            ans = min(ans, freq1[i] + len2 - freq2[i]);
-            ans = min(ans, freq2[i] + len1 - freq1[i]);
+        if(i < 25)
+        {
+            ans = min(ans, freq1[i] + l2 - freq2[i]);
+            ans = min(ans, freq2[i] + l1 - freq1[i]);
         }
     }
     return ans;
