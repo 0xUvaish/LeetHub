f314ac0fe833cc5883326e0f55c472183d0ec696
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-01-18 12:26:36 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (40.97%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.length();
+        vector<int> right(26,-1);
+
+        for(int i = n-1; i >=0 ; i--)
+        {
+            if(right[s[i]-'a'] < 0)
+                right[s[i]-'a'] = i;
+        }
+         
+		
+        vector<int> ans;
+        int start = 0, end = right[s[0]-'a'];
+        
+        for(int i = 1; i < n; i++)
+        {
+            int ch = s[i]-'a';
+           
+            if(i <= end){
+              end = max(end,right[ch]);  
+            }
+            else{
+                ans.push_back(end-start+1);
+                start = i;
+                end = right[ch];
+            }
+        }
+        
+        ans.push_back(end-start+1);
+        
+        return ans;
+    }
+};
