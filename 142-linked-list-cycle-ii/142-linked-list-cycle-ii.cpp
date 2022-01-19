b9602fe933f33414b5df2ee60f0552b6330de4f0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-01-19 12:54:40 +0530

    Time: 12 ms (37.58%), Space: 7.3 MB (99.44%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -13,26 +13,28 @@ public:
     {
         if (!head || !head->next)
             return NULL;
+        
         ListNode *slow = head, *fast = head;
-        // flyod's algorithm to detect cycle in a linked list
+        
         while (fast and fast->next)
         {
             slow = slow->next;
             fast = fast->next->next;
+            
             if (slow == fast)
                 break;
         }
-        // checking whether cycle exist or not
+
         if (slow != fast)
             return NULL;
         
         slow = head;
-        // loop for getting starting node of cycle
         while (slow != fast)
         {
             slow = slow->next;
             fast = fast->next;
         }
+        
         return slow;
     }
 };
