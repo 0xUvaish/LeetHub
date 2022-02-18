5a89be32bb23de28bde5a207bcce440897f1e813
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-02-18 17:38:35 +0530

    Time: 6 ms (55.58%), Space: 7.2 MB (35.82%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        
+        stack<char>s;
+        for(int i=0;i<num.size();i++)
+        {
+            while(k>0 and s.size()>0 and s.top()>num[i])
+            {
+                s.pop();
+                k--;
+            }
+            s.push(num[i]);
+        }
+        
+        while(k>0)
+        {
+            s.pop();
+            k--;
+        }
+
+        string ans=""; 
+        while(!s.empty()){
+            ans += s.top(); 
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
+         int i=0;
+		/* corner case : count the leading number of zeros*/
+        while(ans[i] == '0'){
+            i++;
+        }
+		
+        return (ans.substr(i) == "")? "0": ans.substr(i);
+        
+    }
+};
