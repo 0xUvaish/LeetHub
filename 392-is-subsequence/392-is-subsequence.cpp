14b62b47ef690866642d36fbc4bd3448af9f87f3
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-02 08:47:42 +0530

    Time: 3 ms (47.29%), Space: 6.5 MB (33.59%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int start = 0;
+	    for(char val : t) 
+            start += s[start] == val;
+        
+	return start == s.size();
+    }
+};
