935f0318974992accc01156b89998253c37c04cd
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-07 08:45:47 +0530

    Time: 0 ms, Memory: 5.9 MB - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low = 0, high = x, ans=-1;
+        
+        while(low<=high)
+        {
+            long long mid = low + (high-low)/2;
+            if(mid*mid == x)
+                return ans=mid;
+            else if(mid*mid < x)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            
+            else
+                high = mid - 1;
+        }
+    return ans;
+    }
+};
