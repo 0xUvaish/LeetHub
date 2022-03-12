48f44cfdaebce29c7eb557c8340015978802c5f6
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-12 09:00:50 +0530

    Time: 66 ms (83.90%), Space: 20.8 MB (50.64%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -11,32 +11,41 @@ public:
           
           queue<pair<int,int>> q;
         
-          if(grid[0][0]==1) return -1;
+          if(grid[0][0]==1) 
+              return -1;
+     
           q.push({0,0});
-         
-        
-         dp[0][0]=1;
-        while(!q.empty()){
+          dp[0][0]=1;
+     
+        while(!q.empty())
+        {
 
             pair<int,int> tp=q.front();
             q.pop();
              
-            if(tp.first==n-1 and tp.second==n-1) return dp[n-1][n-1];
+            if(tp.first==n-1 and tp.second==n-1) 
+                return dp[n-1][n-1];
 
-            for(int i=0;i<8;i++){
+            for(int i=0;i<8;i++)
+            {
 
                 int newx=tp.first  + dx[i];
                 int newy=tp.second + dy[i];
 
-                if(newx>=0 and newy>=0 and newx<n and newy<n){
-                   if(dp[newx][newy]==0){
-                        q.push({newx,newy});
+                if(newx>=0 and newy>=0 and newx<n and newy<n)
+                {
+                   if(dp[newx][newy]==0)
+                   {
+                    q.push({newx,newy});
                     dp[newx][newy]=1 + dp[tp.first][tp.second];
-                    if(newx==n-1 and newy==n-1) return dp[n-1][n-1];
+                       
+                    if(newx==n-1 and newy==n-1) 
+                        return dp[n-1][n-1];
                    }
                 }
             }
     }
      return -1;
 }
+    
 };
