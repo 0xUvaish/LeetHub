c8f57bf8071d7083163f90e1593c0b3cc3cdfbd0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-16 19:14:35 +0530

    Time: 332 ms (93.39%), Space: 106.2 MB (78.51%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    
+  public:
+    
+    int minChanges(int src, vector<int> adj[], vector<int> revAdj[], int p)
+    {
+        int changes = 0;
+        for(int child: adj[src])
+        {
+            if(child != p)
+                changes += minChanges(child, adj, revAdj, src) + 1;
+        }
+        
+        for(int child: revAdj[src]) 
+        {
+            if(child != p)
+                changes += minChanges(child, adj, revAdj, src);
+        }
+        
+        return changes;
+    }
+    
+    
+    int minReorder(int n, vector<vector<int>> &connections) 
+    {
+        vector<int> adj[n], revAdj[n];
+        int e = connections.size();
+        
+        for(int i = 0; i < e; i++) 
+        {
+            adj[connections[i][0]].push_back(connections[i][1]);
+            revAdj[connections[i][1]].push_back(connections[i][0]);  
+        }
+        
+        return minChanges(0, adj, revAdj, -1);
+    }
+    
+};
