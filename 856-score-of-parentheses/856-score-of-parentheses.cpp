d01f38b1edf93f8249e281f248f8774c500e137e
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-17 09:49:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -27,7 +27,6 @@ public:
                 }
                 
             }
-            cout<<st.top()<<" ";
         }
         
     int ans=0;   
@@ -39,45 +38,6 @@ public:
         
     return ans;
     }
-    
-    
-//     int scoreOfParentheses(string s) {
-        
-//         stack<int> st;
-        
-//         for(char c:s)
-//         {
-//             if(c=='(')
-//                 st.push(-1);
-//             else
-//             {
-//                 if(st.top()==-1)
-//                 {
-//                     st.pop();
-//                     st.push(1);
-//                 }
-//                 else
-//                 {
-//                     int x=0;
-//                     while(!st.empty() && st.top()!=-1)
-//                     {
-//                         x+=st.top();
-//                         st.pop();
-//                     }
-//                     st.pop();
-//                     st.push(2*x);
-//                 }
-//             }
-//         }
-//         int x=0;
-//         while(!st.empty())
-//         {
-//             x+=st.top();
-//             st.pop();
-//         }
-        
-//         return x;
-//     }
     
     
 };
