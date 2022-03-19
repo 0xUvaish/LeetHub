5d797cd6a622d443ef809825fb5c286eb5a694e0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-19 14:18:33 +0530

    Time: 103 ms (15.41%), Space: 14.7 MB (32.97%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,42 +8,41 @@
  */
 class Solution {
 public:
-// O(n) time complexity O(n) Space complexity
-     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
-     {
-         unordered_map<ListNode*,int> map;
-         while(headA != NULL)
-         {
-             map[headA]++;
-             headA = headA->next;
-         }
-         while(headB != NULL)
-         {
-             if(map.find(headB) != map.end())
-                 return headB;
-             headB = headB->next;
-         }
-         return NULL;        
-     }
+// O(n+m) time complexity O(n) Space complexity
+     // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+     // {
+     //     unordered_map<ListNode*,int> map;
+     //     while(headA != NULL)
+     //     {
+     //         map[headA]++;
+     //         headA = headA->next;
+     //     }
+     //     while(headB != NULL)
+     //     {
+     //         if(map.find(headB) != map.end())
+     //             return headB;
+     //         headB = headB->next;
+     //     }
+     //     return NULL;        
+     // }
     
     // O(1) spaceComplexity O(m + n) time Complexity
-    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-    //     ListNode *a = headA;
-    //     ListNode *b = headB;
-    //     while(a!=b){
-    //         if(a == NULL){
-    //             a = headB;
-    //         }
-    //         else{
-    //             a = a->next;
-    //         }
-    //         if(b == NULL){
-    //             b = headA;
-    //         }
-    //         else{
-    //             b = b->next;
-    //         }
-    //     }
-    //     return a;
-    // }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+        ListNode *a = headA;
+        ListNode *b = headB;
+        
+        while(a!=b)
+        {
+            if(!a)
+                a = headB;
+            else
+                a = a->next;
+            
+            if(!b)
+                b = headA;
+            else
+                b = b->next;
+        }
+        return a;
+    }
 };
