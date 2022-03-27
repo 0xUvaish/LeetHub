082e1960b1298d6b6d3d0535a7db72800918de80
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-27 22:12:12 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,48 +8,73 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int temp;
-        int i=0,j=0,count=0;
-        while(i<n && j<m)
-        {
-            if(arr1[i]<arr2[j])
-            {
-                temp=arr1[i];
-                i++;
-            }
-            else
-            {
-                temp=arr2[j];
-                j++;
-            }
-        count++;
-        if(count==k)
-            return temp;
-        }
+        // int temp;
+        // int i=0,j=0,count=0;
+        // while(i<n && j<m)
+        // {
+        //     if(arr1[i]<arr2[j])
+        //     {
+        //         temp=arr1[i];
+        //         i++;
+        //     }
+        //     else
+        //     {
+        //         temp=arr2[j];
+        //         j++;
+        //     }
+            
+        // count++;
         
-        if(count<k)
-        {
-            while(i<n)
-            {
-                temp=arr1[i];
-                i++;
-                 count++;
-                if(count==k)
-                    return temp;
-            }
-            while(j<m)
-            {
-                temp=arr2[j];
-                j++;
-                 count++;
-                if(count==k)
-                    return temp;
-            }
-        }
+        // if(count==k)
+        //     return temp;
+        // }
         
+        // if(count<k)
+        // {
+        //     while(i<n)
+        //     {
+        //         temp=arr1[i];
+        //         i++;
+        //          count++;
+        //         if(count==k)
+        //             return temp;
+        //     }
+        //     while(j<m)
+        //     {
+        //         temp=arr2[j];
+        //         j++;
+        //          count++;
+        //         if(count==k)
+        //             return temp;
+        //     }
+        // }
+    
+    if(n>m)
+        return kthElement(arr2, arr1, m, n, k);
         
+    int low=max(0,k-m);
+    int high=min(k,n);
+    
+    while(low<=high)
+    {
+        int cut1 = (low + high)/2;
+        int cut2 = k - cut1;
         
+        int l1 = cut1==0 ? INT_MIN: arr1[cut1-1];
+        int l2 = cut2==0 ? INT_MIN: arr2[cut2-1];
+        int r1 = cut1==n ? INT_MAX: arr1[cut1];
+        int r2 = cut2==m ? INT_MAX: arr2[cut2];
         
+        if(l1<=r2 && l2<=r1)
+        return max(l1,l2);
+        
+        else if(l1>r2)
+            high = cut1-1;
+        else
+            low = cut1+1;
+    }
+    
+    return 1;
     }
 };
 
