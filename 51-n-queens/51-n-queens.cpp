05c2b7aad69ea855790b18df7d8062d7b4145057
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-27 11:38:36 +0530

    Time: 4 ms (91.68%), Space: 7.4 MB (55.48%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+vector<vector<string>> ans;
+    bool issafe(vector<string> &v,int i,int j){
+        int x=i-1;
+        while(x>=0){
+            if(v[x][j]=='Q')
+                return false;
+            x--;
+        }
+        x=i-1;
+        int y=j-1;
+        while(x>=0 && y>=0){
+            if(v[x][y]=='Q')
+                return false;
+            x--;
+            y--;
+        }
+        x=i-1;
+        y=j+1;
+        while(x>=0 && y<v.size()){
+            if(v[x][y]=='Q')
+                return false;
+            x--;
+            y++;
+        }
+        return true;
+            
+    }
+    void nqueen(vector<string> &v,int i,int n)
+    {
+        if(i==n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        
+        for(int c=0;c<n;c++)
+        {
+            if(issafe(v,i,c))
+            {
+                v[i][c]='Q';
+                nqueen(v,i+1,n);
+                v[i][c]='.';
+            }
+        }
+            
+    }
+    
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        string s="";
+        for(int i=0;i<n;i++)
+            s+=".";
+        // for(int i=0;i<n;i++)
+        //     v.push_back(s);
+        vector<string> v(n, s);
+        int i=0;
+        nqueen(v,i,n);
+        return ans;
+    }
+};
