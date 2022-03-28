b98f9f02e15322db25bcf33cb219f674d7bf8808
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-28 21:03:42 +0530

    Time: 260 ms (85.94%), Space: 66.9 MB (45.78%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,26 +1,90 @@
-class Solution {
-public:
-    int findMaximumXOR(vector<int>& nums) 
+class Node
+{
+    public:
+    
+    Node *links[2];
+    
+    bool containsKey(int bit)
     {
-        int ans=0,mask=0;
-        unordered_set<int>st;
+        return (links[bit]!=NULL);
+    }
+    
+    Node *get(int bit)
+    {
+        return links[bit];
+    }
+    
+    void put(int bit,Node *node)
+    {
+        links[bit]=node;
+    }
+};
+
+class Trie
+{
+    Node *root;   
+    public:
+    
+    Trie()
+    {
+        root=new Node();
+    }
+    void insert(int num)
+    {
+        Node *temp=root;
         for(int i=31;i>=0;i--)
         {
-            mask|=(1<<i);
-            int temp=ans|(1<<i);
-            for (int j = 0; j < nums.size(); j++) 
+            int bit=(num>>i)&1;
+            if(!temp->containsKey(bit))
             {
-                int num = nums[j] & mask;
-                if (st.find(temp ^ num)!=st.end()) 
-                {
-                    ans = temp;
-                    break;
-                }
-                
-                st.insert(num);
+                temp->put(bit,new Node());
             }
-            st.clear();
+            
+            temp=temp->get(bit);
+                
         }
-        return ans;
+    }
+    
+    int getMax(int num)
+    {
+        Node *temp=root;
+        int maxNum=0;
+        for(int i=31;i>=0;i--)
+        {
+           int bit=(num>>i)&1;
+            int opposite=1-bit;
+            if(temp->containsKey(opposite))
+            {
+                maxNum |= (1<<i);
+                temp=temp->get(opposite);
+            }
+            else
+            {
+                temp=temp->get(bit);
+            }
+        }
+        return maxNum;
+    }
+        
+};
+
+class Solution {
+private : 
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        
+        Trie trie;
+
+        vector<int>nums2=nums;
+        int n=nums.size();
+        int maxVal=0;
+        
+        for(int i=0;i<n;i++)
+            trie.insert(nums[i]);
+        
+        for(int i=0;i<n;i++)
+            maxVal=max(maxVal,trie.getMax(nums2[i]));
+        
+        return maxVal;
     }
 };
