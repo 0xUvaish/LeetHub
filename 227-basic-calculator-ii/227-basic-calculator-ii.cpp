e859aca7169ef0274eb1472f0c2c8193bf829d5d
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-03-29 18:19:22 +0530

    Time: 14 ms (47.67%), Space: 8.9 MB (28.41%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    int calculate(string s) 
+    {
+        s+="+";
+        
+        stack<int> stack;
+        int currNumber  = 0;
+        char sign = '+';
+        
+        for( int i = 0 ; i < s.length() ; i++ )
+        {
+            if( s[i] == ' ' )
+                continue;
+            
+            else if( s[i] >= '0' )
+                currNumber = currNumber*10 + ( s[i] - 48 );
+            
+            else 
+            {
+                if(sign=='+')
+                    stack.push(currNumber);
+                
+                else if(sign=='-')
+                    stack.push(-currNumber );
+                
+                else if(sign=='*')
+                {
+                    int a = stack.top(); stack.pop();
+                    stack.push(a*currNumber);
+                }
+                
+                else if(sign == '/')
+                {
+                    int a = stack.top(); stack.pop();
+                    stack.push(a/currNumber);
+                }
+                
+                currNumber = 0;
+                sign = s[i];
+            }
+        }
+        
+        int result = 0;
+        while(!stack.empty())
+        {
+            result += stack.top();
+            stack.pop();
+        }
+        
+        return result;
+        
+    }
+};
