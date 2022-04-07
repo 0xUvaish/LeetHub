58241cc541c5dcf423958d556df32d1b63d502d8
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-07 12:54:38 +0530

    Time: 1197 ms (24.36%), Space: 59 MB (39.16%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+          
+        int n = points.size();
+        vector<vector<int>> graph(n, vector<int>(n));
+        
+        for(int i=0; i<n; i++) 
+            for(int j=0; j<n; j++)
+                graph[i][j] = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+        
+
+        vector<int>visited;
+        visited.assign(n+1,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        pq.push({0,0});
+        int ans=0;
+      
+        while(!pq.empty())
+        {
+    
+            auto best=pq.top();
+            pq.pop();
+            
+            int src=best.second;
+            int weight=best.first;
+            
+            if(visited[src])
+                continue;
+            
+            visited[src]=1;   
+            ans+=weight;
+            
+            for(int i=0;i<n;i++)
+                if(visited[i]==0)
+                    pq.push(make_pair(graph[src][i],i));
+            
+        }
+        
+return ans;
+    }
+};
