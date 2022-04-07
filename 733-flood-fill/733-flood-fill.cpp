cb7f3300eef99bba794799b1e7f43595f7f7bc55
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-07 09:40:53 +0530

    Time: 13 ms (49.64%), Space: 13.9 MB (80.60%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -5,11 +5,10 @@ public:
     {  
          if(sr<0 || sc<0 || sc>=image[0].size() || sr>=image.size())
              return;
-         if(image[sr][sc] != oldColor || newColor == oldColor)
+         if((image[sr][sc] != oldColor) || (newColor == oldColor))
              return;
       
          image[sr][sc] = newColor;
-      
          floodFill(image, sr, sc-1, newColor, oldColor);
          floodFill(image, sr, sc+1, newColor, oldColor);
          floodFill(image, sr-1, sc, newColor, oldColor);
