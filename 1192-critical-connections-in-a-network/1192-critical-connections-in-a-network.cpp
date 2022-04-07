8c858562132b3a444895a1dba8dae74a534a6ad9
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-07 13:11:05 +0530

    Time: 536 ms (94.41%), Space: 174.5 MB (75.79%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -10,35 +10,35 @@ public:
         }
     }
     // Bridges Algo is here
-    void dfs(vector<int> adj[],int disc[],int low[],bool visited[],int s,int parent,int &time)
+    void dfs(vector<int> adj[],vector<int> &tin,vector<int> &low,vector<bool> &visited,int s,int parent,int &time)
     {
         visited[s] = true;
-        disc[s] = low[s] = ++time;
+        tin[s] = low[s] = ++time;
         
-        for(int &v: adj[s]){
-            if(!visited[v]){
-                dfs(adj,disc,low,visited,v,s,time);
+        for(int &v: adj[s])
+        {
+            if(!visited[v])
+            {
+                dfs(adj,tin,low,visited,v,s,time);
                 low[s] = min(low[s],low[v]);
-                if(low[v] > disc[s])
+                if(low[v] > tin[s])             //If its a bridge
                     ans.push_back({s,v});
             }
             else if(v != parent)
-                low[s] = min(low[s],disc[v]);
+                low[s] = min(low[s],tin[v]);
         }
     }
     
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections)
     {
-        vector<int> adj[n];
-        addEdge(adj,connections);
-        int tin[n];
-        fill(tin,tin+n,0);
-        int low[n];
-        fill(low,low+n,0);
-        bool visited[n];
-        fill(visited,visited+n,false);
+        vector<int> adj[n], tin(n,0), low(n,0);
+        vector<bool> visited(n, false);
+        
+        addEdge(adj,connections);  //Created the graph
         int time = 0;
-        dfs(adj,tin,low,visited,0,-1,time);
+
+        dfs(adj,tin,low,visited,0,-1,time);  //Run Tarjan's Algo
+        
         return ans;
     }
 };
