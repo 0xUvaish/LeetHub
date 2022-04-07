e6bc7703d2a93dae809992ffbff30d1c97afef1f
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-07 14:55:25 +0530

    Time: 16 ms (61.38%), Space: 7.3 MB (84.29%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-int min(int a,int b,int c)
-    {
-        int x=INT_MAX;
-        if(a<x)
-            x=a;
-        if(b<x)
-            x=b;
-        if(c<x)
-            x=c;
-        return x;
-    }
     
     int minDistance(string word1, string word2) 
     {
-        int l1=word1.length(),l2=word2.length();
+        int l1=word1.length();
+        int l2=word2.length();
         int dp[l1+1][l2+1];
         
         for (int i=0;i<=l1;i++)
@@ -23,14 +13,17 @@ int min(int a,int b,int c)
             {
                 if(i==0)
                     dp[i][j]=j;
+                
                 else if(j==0)
                     dp[i][j]=i;
+                
                 else if(word1[i-1]==word2[j-1])
                     dp[i][j]=dp[i-1][j-1];
                 else
-                    dp[i][j]=min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])+1;
+                    dp[i][j]=1 + min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]});
             }
         }
+        
         return dp[l1][l2];
     }
 };
