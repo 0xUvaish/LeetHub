c7740625ef81cc3aa80a03d724cdbde934d7d585
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-14 13:10:55 +0530

    Time: 185 ms (75.35%), Space: 83 MB (78.87%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) 
+    {
+     
+        int n=nums.size();
+        
+        
+        //Some kind of swap sort
+        for(int i=0;i<n;i++)
+        {
+           if((nums[i]<=n && nums[i]>=1) && (nums[i]!=nums[nums[i]-1]))
+           {
+               swap(nums[i],nums[nums[i]-1]);
+                i--; //So as to re process the same index element after swapping
+           }
+            
+        }
+                    //Now checking if the index is having its corresponding element or not
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        
+        return n+1;
+    }
+};
