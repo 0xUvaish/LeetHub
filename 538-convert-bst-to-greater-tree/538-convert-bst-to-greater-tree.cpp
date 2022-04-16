0434c2b0c2a4ee44a80f8f197c53bece0dc884f1
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-16 08:48:41 +0530

    Time: 36 ms (90.34%), Space: 33.4 MB (62.62%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int tsf=0;
+    void help(TreeNode* root)
+    {
+        if(!root)
+            return;
+        
+        help(root->right);
+        root->val += tsf;
+        tsf = root->val;
+        help(root->left);
+    }    
+    
+    TreeNode* convertBST(TreeNode* root) {
+      help(root);
+      return root;
+    }
+};
