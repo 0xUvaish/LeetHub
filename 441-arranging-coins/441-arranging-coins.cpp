dbc4c674f18cf9acaa31e9517f7067306af946e4
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-25 11:00:54 +0530

    Time: 12 ms (28.71%), Space: 5.9 MB (72.33%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+/* sqrt(n) Solution
+int arrangeCoins(int n) {
+       int i=1,rows=0; 
+        while(n>=i)
+        {
+            rows++;
+            n-=i;
+            i++;
+        }
+    return rows;
+    }
+*/
+  
+    
+// O(logn) Solution
+int arrangeCoins(int n)
+{
+    long low=0, high=n, ans=0;
+    while(low<=high)
+    {
+        long mid =(low+high)/2;
+        long count= (mid*(mid+1))/2;
+        
+        if(count==n)
+            return mid;
+        
+        if(count<n)
+        {
+            ans = mid;
+            low= mid+1;
+        }
+        else
+            high = mid-1;
+           
+    }
+    return ans;
+ }
+     
+};
