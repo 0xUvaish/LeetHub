ba40f02928bae84acaedbbf29b06011c1548f740
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-26 12:02:13 +0530

    Time: 1345 ms (5.03%), Space: 189.3 MB (12.74%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> dirs = {{-1,0},{0,-1},{1,0},{0,1}};
     
     bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j){        
-        if( grid2[i][j]==1 && grid1[i][j]==0)
+        if(grid2[i][j]==1 && grid1[i][j]==0)
             return false;
         
         grid2[i][j] = 0;
@@ -13,9 +13,11 @@ public:
         {
         int x = it[0] + i;
         int y = it[1] + j;
+            
         if(x<0 || x>=grid2.size() || y<0 || y>=grid2[0].size() || grid2[x][y]==0)
             continue;
-            val = val & dfs(grid1, grid2, x, y); 
+            
+        val = val & dfs(grid1, grid2, x, y); 
         }
         
         return val;
