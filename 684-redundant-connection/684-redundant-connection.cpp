589eae4e2820135d4ab5301d81feedd491c2094b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-26 11:22:48 +0530

    Time: 8 ms (74.97%), Space: 9 MB (58.37%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-        vector<int> parents;
+    vector<int> parents;
 
     int find_parent(int a)
     {
@@ -10,28 +10,23 @@ public:
             return parents[a] = find_parent(parents[a]);
     }
     
-    void do_union(int node1 , int node2)
+    void do_union(int node1 ,int node2)
     {   
         node1 = find_parent(node1);
         node2 = find_parent(node2);
-        if(node1 == node2)
-            return;
-        else 
-        {               
-            parents[node2] = node1;
-            return;
-        }
+            
+        parents[node2] = node1;        
     }
     
     vector<int> findRedundantConnection(vector<vector<int>>& edges) 
     {
         parents = vector<int>(1001);
-        for(int i = 0 ; i < 1001 ; ++i)
+        for(int i=0; i<1001; ++i)
             parents[i] = i;
             
-        vector<int> answer(2,0);
+        vector<int>answer(2,0);
         
-        for(int i = 0 ; i < edges.size() ; ++i)
+        for(int i=0; i<edges.size(); ++i)
         {
             if(find_parent(edges[i][0]) == find_parent(edges[i][1]))
             {
