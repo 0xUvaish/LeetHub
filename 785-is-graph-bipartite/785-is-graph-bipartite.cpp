daedfff8b6f693e37f488394bbf563c1dce0e33e
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-04-29 10:42:28 +0530

    Time: 34 ms (46.47%), Space: 13.4 MB (93.88%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-bool dfs( int node , vector<vector<int>> &graph , vector<int> &col ,int cur ){
+bool dfs(int node, vector<vector<int>> &graph, vector<int> &col, int cur){
         if(col[node] != -1)
         {
             if(col[node] == cur) 
@@ -11,27 +11,25 @@ bool dfs( int node , vector<vector<int>> &graph , vector<int> &col ,int cur ){
         }
         
         col[node] = cur;
-        
         for(auto it: graph[node])
         {
-            bool f = dfs(it , graph , col  , cur ^ 1);
+            bool f = dfs(it, graph, col, 1-cur);
             if(!f) 
                 return false;
         }
         return true;
     }
     
-bool isBipartite(vector<vector<int>>& graph) {
+bool isBipartite(vector<vector<int>>& graph) 
+{
         int n = graph.size();
-        vector<int> col(n , -1);
+        vector<int> col(n, -1);
         
-        for(int i=0;  i < n ;i++ ){
-            if(col[i] == -1){
-                if(!dfs(i , graph , col , 0)){
+        for(int i=0;  i<n; i++)
+            if(col[i] == -1)
+                if(!dfs(i, graph, col, 0))
                     return false;
-                }
-            }
-        }
+    
         return true;
         
     }
