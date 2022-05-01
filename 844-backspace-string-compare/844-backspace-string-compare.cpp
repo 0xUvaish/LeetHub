0afad13c02cf5d9df05e9c603b32191da1e54d47
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-01 09:08:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -5,9 +5,7 @@ bool backspaceCompare(string s, string t)
         for(int i=0;i<s.size();i++)
         {
             if(s[0]=='#')
-            {
                 s.erase(0,1);
-            }
             
             if(s[i]=='#' && i>=1)
             {
@@ -15,12 +13,11 @@ bool backspaceCompare(string s, string t)
                 i=i-2;
             }
         }
+    
         for(int i=0;i<t.size();i++)
         {
              if(t[0]=='#')
-             {
                 t.erase(0,1);
-             }
            
             if(t[i]=='#' && i>=1)
             {
