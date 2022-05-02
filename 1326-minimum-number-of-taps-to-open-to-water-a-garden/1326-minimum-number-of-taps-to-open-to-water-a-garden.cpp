3b4e8619678aa967c0cef8838bbd585e9ba2a507
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-02 14:13:24 +0530

    Time: 83 ms (22.92%), Space: 26 MB (5.76%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -18,41 +18,77 @@ public:
     
     //Method 2
     //Jump Method to reach target (n index) - O(n) Solution
-    int minTaps(int n, vector<int>& ranges)
-    {
-        for(int i=1; i<ranges.size(); i++)
-        {
-            if(ranges[i]==0)
-                ranges[i] = i;
+    
+//     int minTaps(int n, vector<int>& ranges)
+//     {
+//         for(int i=1; i<ranges.size(); i++)
+//         {
+//             if(ranges[i]==0)
+//                 ranges[i] = i;
             
-            else 
-            {
-                int range = max(0, i-ranges[i]);
-                ranges[range] = max(i+ranges[i], ranges[range]);
-            }
-        }
+//             else 
+//             {
+//                 int range = max(0, i-ranges[i]);
+//                 ranges[range] = max(i+ranges[i], ranges[range]);
+//             }
+//         }
         
-        int maxIndex = 0;
-        int jump = 0;
-        int pos = 0;
+//         int maxIndex = 0;
+//         int jump = 0;
+//         int pos = 0;
         
-        for(int i=0; i<n; i++)
-        {
-            if(maxIndex<i)
-                return -1;
+//         for(int i=0; i<n; i++)
+//         {
+//             if(maxIndex<i)
+//                 return -1;
             
-            maxIndex = max(maxIndex, ranges[i]);
+//             maxIndex = max(maxIndex, ranges[i]);
             
-            if(i==pos)
-            {
-                jump++;
-                pos = maxIndex;
-            }
-        }
+//             if(i==pos)
+//             {
+//                 jump++;
+//                 pos = maxIndex;
+//             }
+//         }
         
-    return maxIndex>=n? jump: -1;
-    }
+//     return maxIndex>=n? jump: -1;
+//     }
     
     //Method 3
-    //Interval Method
+    //Interval Method - O(n) Solution
+    
+    int minTaps(int n, vector<int>& ranges) 
+    {
+        vector <vector<int>> intervals;
+        for (int i = 0; i < ranges.size(); i++)
+        {
+            vector<int> inter{i - ranges[i], i + ranges[i]};
+            intervals.push_back(inter);
+        }
+        
+        sort(intervals.begin(), intervals.end(), [](auto &p, auto &q){
+            return (p[0] < q[0]);
+        });
+        
+        int count = 0, start_time = 0, end_time = 0;
+        
+        int i=0;
+        while (end_time < n) 
+        {
+            while(i<intervals.size() && start_time >= intervals[i][0])
+            {
+                end_time = max(end_time, intervals[i][1]);
+                i++;
+            }
+            
+            if (start_time == end_time)  //end time did not exceed start time 
+                return -1;
+            
+            count++;
+            start_time = end_time;
+        }
+        
+        return count;
+    }
+    
 };
