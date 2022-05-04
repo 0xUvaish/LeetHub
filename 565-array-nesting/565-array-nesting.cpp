685e0b1faaf6728c77c02fac18f6b9bcd80ba6b1
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-04 22:40:31 +0530

    Time: 324 ms (22.18%), Space: 91.3 MB (96.48%) - LeetHub

---
+++ 565-array-nesting/565-array-nesting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // O(N) TC, O(1) SC
+    int arrayNesting(vector<int>& nums) 
+    {
+       
+        int res =0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] != -1)          //Not visited yet
+            {
+                int start = nums[i], count=0;
+                while(nums[start] != -1)
+                {
+                    int tmp = start;
+                    start = nums[start];
+                    count++;
+                    nums[tmp] = -1;
+                }
+                
+                res = max(res, count);
+            }
+        }
+        
+    return res;
+    }
+    
+};
