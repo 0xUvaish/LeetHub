2fc6e07f8798144f6e5763419d9650d430afda74
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-04 18:53:54 +0530

    Time: 3 ms (43.79%), Space: 5.9 MB (59.44%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -25,34 +25,34 @@ public:
 // } 
     
 //Method 2 - DP Solution - O(n)
-    int countVowelStrings(int n) {
-        int dp[n][5];
-        if(n==1)
-            return 5;
+//     int countVowelStrings(int n) {
+//         int dp[n][5];
+//         if(n==1)
+//             return 5;
         
-        for(int i=0;i<5;i++)
-            dp[0][i]=1;
+//         for(int i=0;i<5;i++)
+//             dp[0][i]=1;
         
-        for(int i=1;i<n;i++)
-        {
-                dp[i][0]=dp[i-1][0]+dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
-                dp[i][1]=dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
-                dp[i][2]=dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
-                dp[i][3]=dp[i-1][3]+dp[i-1][4];
-                dp[i][4]=dp[i-1][4];
-        }
+//         for(int i=1;i<n;i++)
+//         {
+//                 dp[i][0]=dp[i-1][0]+dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+//                 dp[i][1]=dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+//                 dp[i][2]=dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+//                 dp[i][3]=dp[i-1][3]+dp[i-1][4];
+//                 dp[i][4]=dp[i-1][4];
+//         }
         
-        int sum=0;
-        for(int j=0;j<5;j++)
-            sum+=dp[n-1][j];
+//         int sum=0;
+//         for(int j=0;j<5;j++)
+//             sum+=dp[n-1][j];
         
-        return sum;
-    } 
+//         return sum;
+//     } 
     
     //Method 3 - PnC - Combination with repetition - !(n-1+r)/!(r)!(n-1) 
     // n = 5, r = n(n provided in question)
     // === ((n+1)*(n+2)*(n+3)*(n+4))/4
-    // int countVowelStrings(int n) {
-    //     return ((n+1)*(n+2)*(n+3)*(n+4))/24;
-    // }
+    int countVowelStrings(int n) {
+        return ((n+1)*(n+2)*(n+3)*(n+4))/24;
+    }
 };
