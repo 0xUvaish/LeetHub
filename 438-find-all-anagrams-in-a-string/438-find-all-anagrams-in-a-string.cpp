76b98f7d9604b6061956ab56ee8cb228b029d011
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-04 19:20:49 +0530

    Time: 40 ms (26.09%), Space: 8.7 MB (38.87%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    //2 pointer Fixed Sliding Window using Map for Pattern - Aditya Verma
 vector<int> findAnagrams(string s, string p) 
 {
+    vector<int>ans;
     unordered_map<char,int>m;
+    
     for(int i=0;i<p.length();i++)
         m[p[i]]++;
+    
     int count=m.size();
-    vector<int>ans;
+    
     int i=0,j=0;
-    int n=s.length()-1;
+    int n=s.length();
     int k=p.length(); //window size
-    while(j<=n)
+    
+    while(j<n)
     {
         if(m.find(s[j])!=m.end()) //if any character in s matches with the p string character
         {
@@ -18,10 +23,12 @@ vector<int> findAnagrams(string s, string p)
             if(m[s[j]]==0)
                 count--;
         }
+        
         if(j-i+1==k) //we might get an ans
         {
             if(count==0)
                 ans.push_back(i);
+            
             if(m.find(s[i])!=m.end())
             {
                 m[s[i]]++;
@@ -31,7 +38,9 @@ vector<int> findAnagrams(string s, string p)
             i++;
         }
         j++;
+        
     }
+    
     return ans;
   }
 };
