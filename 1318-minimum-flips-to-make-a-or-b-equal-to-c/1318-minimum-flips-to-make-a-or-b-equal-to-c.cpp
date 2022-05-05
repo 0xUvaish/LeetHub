e4a2d67d2296a4cbc0a99ce530e9e133e2c8e419
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-05 11:47:28 +0530

    Time: 4 ms (22.96%), Space: 6 MB (30.54%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countBits(int n)
+    {
+        int count=0;
+        while(n>0)
+        {
+            count++;
+            n = n>>1;
+        }
+    return count;     
+    }
+    
+    int minFlips(int a, int b, int c) {
+       
+        int flips=0;
+        int n = max({countBits(a), countBits(b), countBits(c)});
+        
+        for(int i=0;i<n;i++)
+        {
+            
+            if(c&(1<<i))
+            {
+              if((!(a&(1<<i))) && (!(b&(1<<i))))
+                flips++;
+            }
+            else
+            {
+               if(a&(1<<i))
+                   flips++;
+               if(b&(1<<i))
+                   flips++;               
+            }
+        }
+        
+    return flips;
+    }
+};
