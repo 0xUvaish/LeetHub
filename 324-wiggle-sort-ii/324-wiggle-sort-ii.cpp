1896dd121f1da114ac8fcde474d4ca537e749a41
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-06 10:49:14 +0530

    Time: 21 ms (72.15%), Space: 17.8 MB (48.65%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    //Method 1: Sorting + Space --> Insert grrater no. from i=1 till the end of array and then for i=0 till the end
+    //Method 1: Sorting + Space --> Insert greater no. from i=1 till the end of array and then for i=0 till the end
     void wiggleSort(vector<int>& nums) 
     {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>v(nums.begin(), nums.end());
         
-        vector<int> temp(n);
+        sort(v.begin(),v.end());
         
-        int i = 1, j = n-1;
-        while(i<n)
-        {
-            temp[i] = nums[j];
-            i+=2;
-            j--;
-        }
+        int j=n-1;
+        for(int i=1;i<n;i+=2)
+            nums[i]=v[j--];
         
-        i = 0;
-        while(i<n)
-        {
-            temp[i] = nums[j];
-            i+=2;
-            j--;
-        }
+        for(int i=0;i<n;i+=2)
+            nums[i]=v[j--];
         
-        for(int it=0; it<n; it++)
-            nums[it] = temp[it];
     }
 };
