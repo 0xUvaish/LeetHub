348fb18dbf1bae801e5a5a3ab7534dde8a76ee1d
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-06 20:02:09 +0530

    Time: 23 ms (35.48%), Space: 21.3 MB (6.77%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     //Method 1: Direct STL 
-    bool isMatch(string s, string p) 
-    {
-	regex obj(p);
-	if(regex_match(s, obj))
-		return true;
-	else
-		return false;
-    }
+	// bool isMatch(string s, string p) 
+	// {
+	// regex obj(p);
+	// if(regex_match(s, obj))
+	// 	return true;
+	// else
+	// 	return false;
+	// }
     
     //Method 2: 
+    
+    bool dp(int i, int j, string s, string p, vector<vector<int>> &memo)
+    {
+        if(memo[i][j]!=-1)
+            return (memo[i][j] == 1);
+        
+        bool ans;
+        if(j==p.length())
+            ans = i == s.length();
+        else
+        {
+            bool fchar =  (i<s.length() && (p[j]==s[i] || p[j]=='.'));
+            if(j+1<p.length() && p[j+1]=='*')
+                ans = (dp(i, j+2, s, p, memo)||(fchar && dp(i+1, j, s, p, memo)));
+            else
+                ans = fchar && dp(i+1, j+1, s, p, memo);
+        }
+        memo[i][j] = ans?1:0;
+        return ans;
+    }
+    bool isMatch(string s, string p) 
+    {
+        vector<vector<int>> memo(s.length()+1, vector<int>(p.length()+1, -1));
+        return dp(0, 0, s, p, memo);
+    }
 };
