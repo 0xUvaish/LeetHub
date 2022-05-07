ceb50a5097a84dfa4800dc740ccabf976392b623
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 10:45:23 +0530

    Time: 246 ms (22.40%), Space: 20 MB (68.06%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-    vector<vector<int>> res;
+        int n = nums.size();
+        vector<vector<int>> res;
        
-        if(nums.size() < 3)
+        if(n < 3)
             return res;
         
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size()-2; i++)
+        for(int i=0; i<n-2; i++)
         {
             
-            if(i==0 || nums[i] != nums[i-1])
+            if(i==0 || nums[i] != nums[i-1])                             // i repetition
             {
-                int low = i+1, high = nums.size()-1, sum = 0-nums[i];
+                int low = i+1, high = n-1, sum = 0-nums[i];
                 while(low < high)
                 {
                     if(nums[low] + nums[high] == sum)
                     {
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[low]);
-                        temp.push_back(nums[high]);
-                        res.push_back(temp);
+                        res.push_back({nums[i],nums[low],nums[high]});
                         
-        //Now move low forward and high backwards, until they skip the current element(if repeated)
-                        while(low<high && nums[low] == nums[low+1])
+                        while(low<high && nums[low] == nums[low+1])       //low repetition
                             low++;
-                        while(low<high && nums[high] == nums[high-1])
+                        while(low<high && nums[high] == nums[high-1])     //high repetition
                             high--;
                         
                         low++;
