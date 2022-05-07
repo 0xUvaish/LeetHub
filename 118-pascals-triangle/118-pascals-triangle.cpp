c7f0e127bbc101bef142494f21294fa8f3ad272a
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 09:54:34 +0530

    Time: 4 ms (21.15%), Space: 6.5 MB (33.94%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -9,9 +9,7 @@ public:
         {
             for(int j=0;j<=i;j++)
             {
-                if(j==0)
-                    ans[i].push_back(1);
-                else if(j==i)
+                if(j==0 || j==i)
                     ans[i].push_back(1);
                 else
                     ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
