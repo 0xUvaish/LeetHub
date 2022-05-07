cf16b276bea67b6de54281778d0a2bdd5f82fee7
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 17:56:56 +0530

    Time: 3 ms (96.71%), Space: 11 MB (53.88%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,24 +3,23 @@ class Solution {
 public:
      vector<vector<int>>result;
      void solve(vector<int>& candidates, int target, int sum, vector<int>& temp, int idx){
-        
-         if(sum>target)
-            return;
     
         if(sum==target)
         {
             result.push_back(temp);
             return;
         }
+        else if(sum>target)
+            return;
          
         for(int i=idx;i<candidates.size();i++)
         {
-                temp.push_back(candidates[i]);
-                solve(candidates, target, sum+candidates[i], temp, i); //index i (not i+1) because we can take more than once
-                temp.pop_back();
+            temp.push_back(candidates[i]);
+            solve(candidates, target, sum+candidates[i], temp, i); //index i (not i+1) because we can take more than once
+            temp.pop_back();
         }
-         return;
      }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
         vector<int> temp;
