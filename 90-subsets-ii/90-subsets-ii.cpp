2639a2d375e8bd8737dc0897a35821ccae30d77a
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 18:17:46 +0530

    Time: 3 ms (79.20%), Space: 7.5 MB (73.68%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -4,6 +4,7 @@ public:
     void fun(int idx,vector<int>& a,vector<int>& ds,vector<vector<int>>& ans){
         
         ans.push_back(ds);
+        
         for(int i=idx;i<a.size();i++)
         {
             if(i!=idx && a[i]==a[i-1])
@@ -17,11 +18,13 @@ public:
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& a) {
+        
         vector<vector<int>> ans;
         vector<int> ds;
-        sort(a.begin(),a.end());
-        fun(0,a,ds,ans);
         
+        sort(a.begin(),a.end());
+        
+        fun(0,a,ds,ans);
         return ans;
     }
 };
