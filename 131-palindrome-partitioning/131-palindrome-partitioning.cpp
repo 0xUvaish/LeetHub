d49e5bc35a2fb11b984093b6397f54e4c026157b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 18:08:45 +0530

    Time: 390 ms (11.88%), Space: 144.9 MB (19.20%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -12,33 +12,34 @@ bool check(string s , int l ,int r)
         return true;
     }
     
-    void helper(string s ,int l ,int r,vector<string> temp,vector<vector<string>> &ans){
+    void helper(string s ,int l ,int n,vector<string> temp,vector<vector<string>> &ans){
     
-        if(l>r)
+        if(l==n)
         {
             ans.push_back(temp);
 			return;
         }
         
-        for(int i=l;i<=r;i++)
+        for(int i=l;i<n;i++)
         {
             if(check(s,l,i))
             {
                 temp.push_back(s.substr(l,i-l+1));
-                helper(s,i+1,r,temp,ans);
+                helper(s,i+1,n,temp,ans);
                 temp.pop_back();
             }
             
         }
-        
-        return;
     }
     
     vector<vector<string>> partition(string s)
     {
+        int n = s.size();
+        
         vector<vector<string>> ans;
         vector<string> temp;
-        helper(s,0,s.size()-1,temp,ans);
+        
+        helper(s,0,n,temp,ans);
         return ans;
         
     }
