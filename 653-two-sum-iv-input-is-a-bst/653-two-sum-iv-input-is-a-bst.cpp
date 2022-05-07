196730357cf32cfc79e8717ddb39abf91acac53d
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-07 22:07:09 +0530

    Time: 48 ms (68.19%), Space: 40.9 MB (10.56%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -13,9 +13,13 @@ class Solution {
 public:
     unordered_map<int,int>m;
     bool findTarget(TreeNode* root, int k) {
-        if(!root) return false;
-        if(m[k-root->val]) return true;
+        if(!root) 
+            return false;
+        
+        if(m[k-root->val]) 
+            return true;
         m[root->val]=1;
+        
         return findTarget(root->left, k)||findTarget(root->right,k);
     }
     
