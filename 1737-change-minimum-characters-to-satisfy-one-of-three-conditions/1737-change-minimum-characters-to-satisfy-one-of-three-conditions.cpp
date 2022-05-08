8b0ac9f45bf733c204225e143555e13ee81e22fa
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-08 19:42:32 +0530

    Time: 487 ms (5.60%), Space: 14.9 MB (52.00%) - LeetHub

--- 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
+++ 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     
-int minCharacters(string str1, string str2)
-{
-    int l1 = (int) str1.length(), l2 = (int) str2.length();
-    vector<int> freq1(26, 0), freq2(26, 0);
-    int ans = l1 + l2;
-    
-    for(auto x: str1)
-        freq1[x-'a']++;
-    
-    for(auto x: str2)
-        freq2[x-'a']++;
-    
-    for(int i = 0; i < 26; i++)
+    int count(string &a,string &b)
     {
-        ans = min(ans, l1 - freq1[i] + l2 - freq2[i]);
+        int ans=INT_MAX;
         
-        if(i > 0)
+        for(char ch='b';ch<='z';ch++)
         {
-            freq1[i] += freq1[i-1];
-            freq2[i] += freq2[i-1];
+            int cnt=0;
+            
+            for(char it:a)
+                cnt += (it>=ch);
+            for(char it:b)
+                cnt += (it<ch);
+            
+            ans=min(ans,cnt);
         }
-        if(i < 25)
-        {
-            ans = min(ans, freq1[i] + l2 - freq2[i]);
-            ans = min(ans, freq2[i] + l1 - freq1[i]);
-        }
+        
+        return ans;
     }
-    return ans;
-}
+    
+    int minCharacters(string a, string b) 
+    {
+        int x=count(a,b);  //Condition 1
+        int y=count(b,a);  //Condition 2
+        
+        int fans=INT_MAX;
+        
+        for(char c='a';c<='z';c++)   //Condition 3
+        {
+            int cnt=0;
+            
+            for(char it:a)
+                cnt+=(it!=c);
+            for(char it:b)
+                cnt+=(it!=c);
+            
+            fans=min(fans,cnt);  
+        }
+        
+     return min({x,y,fans});
+    }
     
 };
