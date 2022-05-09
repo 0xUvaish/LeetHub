ecfc7d14b6523e2af7fde5c059296d2fc8dabd44
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-09 08:35:42 +0530

    Time: 5 ms (16.72%), Space: 6.4 MB (84.83%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -9,24 +9,32 @@ public:
         }
         int digit=digits[index]-'0';
         int len=mapping[digit].size();
+        
         for(int i=0;i<len;i++)
-        {
             combo(digits,index+1,res+mapping[digit][i],ans,mapping,size);
-        }
+        
     }
-    vector<string> letterCombinations(string digits) {
-        vector<char> mapping[]={{},{},{'a','b','c'},{ 'd', 'e', 'f' },
+    
+    vector<string> letterCombinations(string digits)
+    {
+        vector<char> mapping[]=
+        {
+        {},{},{'a','b','c'},{ 'd', 'e', 'f' },
         { 'g', 'h', 'i' },
         { 'j', 'k', 'l' },
         { 'm', 'n', 'o' },
         { 'p', 'q', 'r', 's'},
         { 't', 'u', 'v' },
-        { 'w', 'x', 'y', 'z'}};
+        { 'w', 'x', 'y', 'z'}
+        };
+        
         string res;
         vector<string> ans;
+        
         int size=digits.size();
         if(size==0)
-            return {};
+            return ans;
+        
         combo(digits,0,res,ans,mapping,size);
         return ans;
     }
