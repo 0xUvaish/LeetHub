0db61329d9fb5e09ac7eff97b5e1a937f4455768
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-10 18:21:16 +0530

    Time: 1166 ms (21.61%), Space: 59 MB (35.56%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
     
-    //Prims Algorithms using Priorirty Queue
+    //Prims Algorithms using Priorirty Queue - PepCoding Method
     int minCostConnectPoints(vector<vector<int>>& points) {
           
         int n = points.size();
-        vector<vector<int>> graph(n, vector<int>(n));
+        vector<vector<int>> graph(n, vector<int>(n)); //Adjacency Matrix
+        
         
         for(int i=0; i<n; i++) 
+        {
             for(int j=0; j<n; j++)
+            {
+                if(i==j)                //As distance will be zero anyways
+                    continue;
                 graph[i][j] = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+            }
+        } 
         
 
         vector<int> visited(n,0);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
 
+        //Weight of edge, Node reachable using that edge
         pq.push({0,0});
         int ans=0;
       
         while(!pq.empty())
         {
-    
             auto best=pq.top();
             pq.pop();
             
-            int src=best.second;
+            int node=best.second;
             int weight=best.first;
             
-            if(visited[src])
+            if(visited[node])
                 continue;
             
-            visited[src]=1;   
+            visited[node]=1;   
             ans+=weight;
             
             for(int i=0;i<n;i++)
                 if(!visited[i])
-                    pq.push(make_pair(graph[src][i],i));
+                    pq.push({graph[node][i],i});
             
         }
         
