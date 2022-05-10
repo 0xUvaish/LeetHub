47f8f2ceeb33cacdc96afabaa1d20b769a112336
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-10 19:58:28 +0530

    Time: 80 ms (69.84%), Space: 61.6 MB (70.83%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -4,16 +4,18 @@ public:
         int n=nums.size();
         if(n<3) 
             return false;
+        
         int a1=nums[0],a2=INT_MAX;
         
         for(int i=1;i<n;i++)
         {
-            if(nums[i]>a2)
+            if(nums[i]>a2)            //Found a3
                 return true;
             
-            if(nums[i]>a1)
+            if(nums[i]>a1)            //Need to update a2
                 a2=min(a2,nums[i]);
-            else a1=nums[i];
+            else 
+                a1=nums[i];           //Need to update a1
         }
         return false;
     }
