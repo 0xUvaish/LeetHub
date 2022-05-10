68ee2b0eaae72d878640e6174942ec71ad259031
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-10 12:57:50 +0530

    Time: 20 ms (89.76%), Space: 14 MB (56.41%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -12,23 +12,23 @@ public:
             digraph[edge[1]].push_back(edge[0]);
         }
         
-        stack<int> st;
+        queue<int> qu;
         for (int i = 0; i < numCourses; ++i) {
             if (indegree[i] == 0) {
-                st.push(i);
+                qu.push(i);
             }
         }
         
         int studied = 0;
-        while (!st.empty()) {
-            int currCourse = st.top();
-            st.pop();
+        while (!qu.empty()) {
+            int currCourse = qu.front();
+            qu.pop();
             
             ++studied;
             for (int& nextCourse : digraph[currCourse]) {
                 --indegree[nextCourse];
                 if (indegree[nextCourse] == 0) {
-                    st.push(nextCourse);
+                    qu.push(nextCourse);
                 }
             }
             
