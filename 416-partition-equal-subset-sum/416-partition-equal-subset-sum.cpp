6a7376dfaf902613ee2e1b0a49d40c8d763f4b35
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-11 09:47:26 +0530

    Time: 265 ms (45.01%), Space: 11.2 MB (73.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         bool t[n+1][sum+1];
         
-        for(int i=0;i<n+1;i++)
+        for(int i=0;i<=n;i++)
         {
-            for(int j=0;j<sum+1;j++)
+            for(int j=0;j<=sum;j++)
             {
                 if(i==0)
                     t[i][j] = false;
