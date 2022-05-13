60b929a7f809a95802b46ef3e68d32e8b20466f0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-13 16:53:34 +0530

    Time: 0 ms (100.00%), Space: 7 MB (54.23%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
+    //Method 1: stringstream object
  int myAtoi(string s) 
  {
        stringstream abc(s);
         int x=0;
         abc>>x;
-        
-        return x;
+     return x;
     }
 };
