3ff828869bdcd69b52fddd6fc2d55a8efb9592cc
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-13 18:34:20 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.78%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        for (int i = 1; i < nums.size(); i++)
+            nums[i] = max(nums[i - 1], (i != 1 ? nums[i - 2] : 0) + nums[i]);
+        
+    return nums[nums.size() - 1];  
+    }
+};
