4b4604a1ac15ffd1d6c319ee6c5962f02feba58d
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-15 16:13:38 +0530

    Time: 4 ms (36.48%), Space: 6.4 MB (71.23%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     bool isSubsequence(string s, string t) {
         int start = 0;
 	    for(char val : t) 
-            start += s[start] == val;
+            start += (s[start] == val);
         
 	return start == s.size();
     }
