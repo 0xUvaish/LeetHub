85d01c7740f1c3a542ac125766becb8509126f98
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-15 16:01:47 +0530

    Time: 12 ms (45.07%), Space: 10.6 MB (17.06%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -7,7 +7,8 @@ public:
         int sum = 0, mn = INT_MAX;
         int i = 0;
         
-        for(int j=0 ; j<nums.size() ; j++){
+        for(int j=0 ; j<nums.size() ; j++)
+        {
             sum += nums[j];
             
             if(sum >= k)
@@ -15,7 +16,8 @@ public:
                 while(sum >= k)
                 {
                     mn = min(mn, j-i+1);
-                    sum = sum - nums[i++];
+                    sum = sum - nums[i];
+                    i++;
                 }
             }
         }
