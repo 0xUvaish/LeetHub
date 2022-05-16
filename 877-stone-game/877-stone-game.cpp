5490d3a7953104292dd5062e6ec113729e4e314c
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-16 12:07:06 +0530

    Time: 37 ms (20.94%), Space: 16.5 MB (13.44%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    //Method 1: Even Size -> Therefore always be a winner as Alice > Bob stones in total
+    // bool stoneGame(vector<int>& piles) {
+    //    return true; 
+    // }
+    
+    //Method 2: Implementation: DP Memoization
+    int help(vector<int> &piles, int start, int end, vector<vector<int>> &dp)
+    {
+        if(start>end)
+            return 0;
+        if(dp[start][end]!=-1)
+            return dp[start][end];
+        
+        int l1 = piles[start] + min(help(piles, start+2, end, dp), help(piles, start+1, end-1, dp));
+        int r1 = piles[end] + min(help(piles, start+1, end-1, dp), help(piles, start, end-2, dp));
+        
+        return dp[start][end]=max(l1,r1);
+    }
     bool stoneGame(vector<int>& piles) {
-       return true; 
+       int total=0,Alice=0, Bob=0;
+        int n = piles.size();
+        for(auto x: piles)
+            total += x;
+    
+     vector<vector<int>> dp(n, vector<int> (n,-1));
+     Alice = help(piles, 0, n-1, dp);
+     Bob = total - Alice;
+     return Alice>Bob;
     }
 };
