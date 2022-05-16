5139e458fd564fa1d54cf0be39706d95214ea3fd
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-16 10:04:26 +0530

    Time: 4 ms (52.09%), Space: 6.1 MB (41.97%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -1,41 +1,93 @@
 class Solution {
 public:
+// bool isNumber(string s) 
+// {
+//         bool dig = false, exp = false, dot = false;
+    
+//         for (int i = 0; i < s.size(); ++i)
+//         {
+//             char curr = s[i];
+            
+//             if(isdigit(curr)) 
+//                 dig = true;
+            
+//             else if(curr == '-' || curr == '+') 
+//             {
+//                 if (i != 0 && s[i - 1] != 'e' && s[i - 1] != 'E') 
+//                     return false;
+//             } 
+            
+//             else if (curr == 'e' || curr == 'E') 
+//             {
+//                 if(exp || !dig) 
+//                     return false;
+//                 exp = true;
+//                 dig = false;
+//             }
+            
+//             else if (curr == '.')
+//             {
+//                 if(dot || exp) 
+//                     return false;
+//                 dot = true;
+//             }
+            
+//             else  
+//                 return false;
+//         }
+    
+//     return dig;
+//     }
+    
 bool isNumber(string s) 
-{
-        bool dig = false, exp = false, dot = false;
+ {
+    int i=0, n = s.length();
     
-        for (int i = 0; i < s.size(); ++i)
-        {
-            char curr = s[i];
-            
-            if(isdigit(curr)) 
-                dig = true;
-            
-            else if(curr == '-' || curr == '+') 
-            {
-                if (i != 0 && s[i - 1] != 'e' && s[i - 1] != 'E') 
-                    return false;
-            } 
-            
-            else if (curr == 'e' || curr == 'E') 
-            {
-                if(exp || !dig) 
-                    return false;
-                exp = true;
-                dig = false;
-            }
-            
-            else if (curr == '.')
-            {
-                if(dot || exp) 
-                    return false;
-                dot = true;
-            }
-            
-            else  
-                return false;
-        }
+    while(i<n && s[i]==' ')
+        i++;
+    if(i<n && (s[i]=='+' || s[i]=='-'))
+        i++;
     
-    return dig;
+    bool isNumeric = false;
+    while(i<n && (s[i]>='0' && s[i]<='9'))
+    {
+        cout<<s[i]<<" ";
+        i++;
+        isNumeric = true;
     }
+    
+    if(i<n && s[i]=='.')
+    {
+        i++;
+        while(i<n && (s[i]>='0' && s[i]<='9'))
+        {
+            i++;
+            isNumeric = true;
+        }
+    }
+    
+    if(i<n && isNumeric && (s[i]=='e' || s[i]=='E'))
+    {
+        i++;
+        isNumeric = false;
+        
+        if(i<n && (s[i]=='+' || s[i]=='-'))
+            i++;
+        
+        while(i<n && (s[i]>='0' && s[i]<='9'))
+        {
+            cout<<s[i]<<" ";
+            i++;
+            isNumeric = true;
+        }
+    }
+    
+    
+    while(i<n && s[i]==' ')
+        i++;
+    
+    cout<<i<<" ";
+    return (isNumeric&& i==n);
+    
+ }
 };
