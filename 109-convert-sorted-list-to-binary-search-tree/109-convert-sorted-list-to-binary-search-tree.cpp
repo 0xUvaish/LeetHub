32998ca069688064f506b353a6c20e2f593ab944
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-16 11:08:49 +0530

    Time: 32 ms (63.57%), Space: 28.3 MB (61.50%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -21,39 +21,37 @@
  */
 class Solution {
 public:
-    TreeNode* createTree(vector<int>& temp, int start, int end) 
-    {
-        if (start > end)
-            return NULL;
+    //Method 1: O(n) TC, O(n) SC
+//     TreeNode* createTree(vector<int>& temp, int start, int end) 
+//     {
+//         if (start > end)
+//             return NULL;
         
-        int mid = start + (end - start) / 2;
-        TreeNode* root = new TreeNode(temp[mid]);
+//         int mid = start + (end - start) / 2;
+//         TreeNode* root = new TreeNode(temp[mid]);
         
-        root -> left = createTree(temp, start, mid - 1);
-        root -> right = createTree(temp, mid + 1, end);
+//         root -> left = createTree(temp, start, mid - 1);
+//         root -> right = createTree(temp, mid + 1, end);
         
-        return root;
-    }
-    TreeNode* sortedListToBST(ListNode* head) 
-    {
-        if (!head)
-            return nullptr;
-        if (!head -> next)
-            return new TreeNode(head -> val);
-        vector<int> temp;
-        while (head) 
-        {
-            temp.push_back(head -> val);
-            head = head -> next;
-        }
+//         return root;
+//     }
+//     TreeNode* sortedListToBST(ListNode* head) 
+//     {
+//         if (!head)
+//             return nullptr;
+//         if (!head -> next)
+//             return new TreeNode(head -> val);
+//         vector<int> temp;
+//         while (head) 
+//         {
+//             temp.push_back(head -> val);
+//             head = head -> next;
+//         }
         
-        return createTree(temp, 0, temp.size() - 1);
-    }
-};
-
-
-// class Solution {
-// public:
+//         return createTree(temp, 0, temp.size() - 1);
+//     }
+   
+    //Method 2: O(nlogn) TC, O(logn) SC - Preorder traversal while creating
 //     TreeNode* sortedListToBST(ListNode* head) {
 //         if (!head)
 //             return nullptr;
@@ -72,4 +70,49 @@ public:
 //         root -> right = sortedListToBST(mid -> next);
 //         return root;
 //     }
-// };
+    
+    
+    //Method 3: O(n) TC, O(logn) SC - Inorder Traversal while creating
+    TreeNode* helper(int start, int end, ListNode* &tmp)
+    {
+        if(start>end)
+            return NULL;
+        
+        int mid = (start + end)/2;
+        
+        TreeNode* leftChild = helper(start, mid-1, tmp);
+        
+        TreeNode* parent = new TreeNode(tmp->val);
+        parent->left = leftChild;
+        tmp = tmp->next;
+        
+        parent->right = helper(mid+1, end, tmp);
+        
+        return parent;
+    }
+    
+    
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head)
+            return NULL;
+        if(!head->next)
+            return new TreeNode(head->val);
+        
+        int size=0;
+        ListNode* curr = head;
+        
+        while(curr)
+        {
+            size++;
+            curr = curr->next;
+        }
+        
+        ListNode* tmp = head;
+        
+        int start = 0;
+        int end = size-1;
+        
+        return helper(start, end, tmp);
+        
+    }
+};
