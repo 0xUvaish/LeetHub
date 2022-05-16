68b42ceaca869d55615b44557dc61ae833af0d6b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-16 09:47:34 +0530

    Time: 3 ms (66.17%), Space: 6 MB (76.50%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -8,10 +8,10 @@ bool isNumber(string s)
         {
             char curr = s[i];
             
-            if (isdigit(curr)) 
+            if(isdigit(curr)) 
                 dig = true;
             
-            else if (curr == '-' || curr == '+') 
+            else if(curr == '-' || curr == '+') 
             {
                 if (i != 0 && s[i - 1] != 'e' && s[i - 1] != 'E') 
                     return false;
@@ -19,7 +19,7 @@ bool isNumber(string s)
             
             else if (curr == 'e' || curr == 'E') 
             {
-                if (exp || !dig) 
+                if(exp || !dig) 
                     return false;
                 exp = true;
                 dig = false;
@@ -27,7 +27,7 @@ bool isNumber(string s)
             
             else if (curr == '.')
             {
-                if (dot || exp) 
+                if(dot || exp) 
                     return false;
                 dot = true;
             }
