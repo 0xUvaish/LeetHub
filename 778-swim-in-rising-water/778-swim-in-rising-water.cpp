b1fd47a6ec0572c5ed26693ca6657d553776c8b8
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-19 10:17:24 +0530

    Time: 22 ms (76.29%), Space: 9.4 MB (66.59%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    bool dfs(int x, int y, vector<vector<int>> &heights, int diff, vector<vector<bool>> &vis)
+    {
+    if (x < 0 || y < 0 || x >= heights.size() || y >= heights[0].size() || vis[x][y] == true ||heights[x][y] > diff)
+            return false;
+        
+        vis[x][y] = true;
+        
+        if(x == heights.size()-1 && y==heights[0].size()-1)
+            return true;
+        
+        for (int i = 0; i < 4; i++)
+            if(dfs(x + dir[i][0], y + dir[i][1], heights, diff, vis))
+                return true;
+        
+        return false;
+    }
+    
+    int swimInWater(vector<vector<int>> &heights)
+    {
+            int left = 0, right = 2500, ans = 0, mid;
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+                vector<vector<bool>> vis(heights.size(), vector<bool>(heights[0].size(), false));
+				if (dfs(0, 0, heights, mid, vis))
+                {
+                    ans = mid;
+                    right = mid - 1;
+                }
+                else
+                    left = mid + 1;
+            }
+            return ans;
+    }
+};
