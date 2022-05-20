b8617218a4b5fd8ebad6e4b779098491961390c3
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-05-20 15:45:31 +0530

    Time: 88 ms (79.74%), Space: 41.9 MB (54.14%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
+        
         int i=0, j=people.size()-1,count=0;
         
         while(i<=j)
@@ -12,6 +13,7 @@ public:
             j--;
             count++;
         }
+        
     return count;
     }
 };
