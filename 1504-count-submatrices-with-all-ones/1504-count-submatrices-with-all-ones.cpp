0ec379c4b23d473278b977e8536da4638a553481
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-06-06 12:20:06 +0530

    Time: 128 ms (31.72%), Space: 15.2 MB (39.07%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //Method 1: n^4 Brute Force
+    //Method 2 : n^3 Sol Optimised
     int numSubmat(vector<vector<int>>& matrix) 
     {
         
@@ -16,22 +18,20 @@ public:
         {            
             for (int j = 0; j < n; j++) 
             {
-                if(!j || !matrix[i][j])    // update dp horizontally in the range of j 
-                    dp[i][j] = matrix[i][j];     // if we get a 0 we put a 0      
+                if(!j || !matrix[i][j])
+                    dp[i][j] = matrix[i][j];
                 else 
-                    dp[i][j] =  1 + dp[i][j - 1];   // if not a 0 we add 1 in the last dp
-// basically this is the width of a retangle with one 1 unit breadth
-// we travel all the rows and calcluate the width of rectangle possible in each row (rectangle will be in that row only breadth = 1)
+                    dp[i][j] =  1 + dp[i][j - 1];
                           
-                int width = dp[i][j];       // stores the width of that cell
+                int width = dp[i][j];
                 for(int k = i; k >= 0; k--)
-                {        // travel the previous rows to get a combination of breadth
-                    width = min(dp[k][j], width); // to form a rectangle all rows should have same width 
-// min width and inc breadth will form a rectangle 
-                    ans += width;  // this will add all the possible rectangles as we inc rows and colunmns
+                {
+                    width = min(dp[k][j], width);
+                    ans += width;
                 }
             }
         }
         return ans;
     }
+    
 };
