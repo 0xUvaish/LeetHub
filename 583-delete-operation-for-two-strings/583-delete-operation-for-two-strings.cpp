38b6ab81b5ca37051e38f98664efa94aa212688c
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-06-14 09:14:26 +0530

    Time: 27 ms (58.62%), Space: 7.6 MB (71.41%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -6,29 +6,24 @@ public:
         int dp[s1+1][s2+1]; 
         
         for(int i=0; i<=s1; i++)
-        {
             for(int j=0; j<=s2; j++)
-            {
                 if(i==0 || j==0)
-                { dp[i][j] = 0; }
-            }
-        }
+                    dp[i][j] = 0;
+        
         for(int i=1; i<=s1; i++)
         {
             for(int j=1; j<=s2; j++)
             {
                 if(text1[i-1] == text2[j-1])
-                {
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
+                else
                     dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
-                }
             }
         }
 
-        return dp[s1][s2] ;
+        return dp[s1][s2];
     }
+    
     int minDistance(string word1, string word2) 
     {
         int w1 = word1.size(); int w2 = word2.size();
