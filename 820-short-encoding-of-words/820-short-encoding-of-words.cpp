af726f290190ceeeb313e6975bd94bbf047df29d
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-06-20 10:04:50 +0530

    Time: 81 ms (67.82%), Space: 73.8 MB (29.70%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -8,7 +8,7 @@ private:
     Trie *root = new Trie();
     bool checkWordNotExists(string w) 
     {
-        bool res = false;
+        bool flag = false;
         
         Trie *curr = root;
         for (int i = w.size() - 1, c; i >= 0; i--) 
@@ -16,26 +16,26 @@ private:
             c = w[i] - 'a';
             if (!curr->children[c]) 
             {
-                res = true;
+                flag = true;
                 curr->children[c] = new Trie();
             }
             curr = curr->children[c];
         }
-        return res;
+        return flag;
     }
     
 public:
     int minimumLengthEncoding(vector<string>& words)
     {
 
-        int res = 0;
+        int len = 0;
 
         sort(begin(words), end(words), [](string a, string b){return b.size() < a.size();});
 
         for (string w: words)
             if (checkWordNotExists(w)) 
-                res += w.size() + 1;
+                len += w.size() + 1;
         
-        return res;
+        return len;
     }
 };
