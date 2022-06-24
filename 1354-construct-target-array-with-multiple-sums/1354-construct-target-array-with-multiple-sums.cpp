10898349a183105f8807ee9113691419f408834b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-06-24 10:25:04 +0530

    Time: 69 ms (44.06%), Space: 30.1 MB (37.06%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        if(target.size()==1)
+           return (target[0]==1);
+        
+        priority_queue<int> pq;
+        long long tsum = 0;
+        for(auto it : target)
+        {
+            tsum += it;
+            pq.push(it);
+        }
+        
+        long long maxEle;
+        while(!pq.empty() && pq.top()!=1)
+        {
+            maxEle = pq.top();
+            pq.pop();
+            long long rsum = tsum-maxEle;
+            long long element;
+            
+            if(maxEle<2*rsum)
+                element = maxEle-rsum;
+            else
+            {
+                long long multi = 1;
+                if(maxEle%2==0)
+                    multi = (maxEle/rsum)-1;
+                else
+                    multi = maxEle/rsum;
+                
+                element = maxEle-multi*rsum;
+            }
+            
+            if(element<=0) 
+                return false;
+            
+            pq.push(element);
+            tsum = rsum+element;
+        }
+        return true;
+    }
+};
