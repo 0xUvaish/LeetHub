40b1447f9c64f2bbc562abbf3d729f3ee471f98a
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-06-30 10:45:27 +0530

    Time: 68 ms (39.25%), Space: 28.3 MB (83.17%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     //M1: Sorting TC O(nlogn)
-    int minMoves(vector<int>& nums) {
+    // int minMoves(vector<int>& nums) {
+    //     int ans=0;
+    //     int n=nums.size();
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=1;i<n;i++)
+    //         ans+=nums[i]-nums[0];
+    //     return ans;
+    // }
+    
+    //M2: Find Min directly O(n)
+        int minMoves(vector<int>& nums) {
         int ans=0;
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++)
-            ans+=nums[i]-nums[0];
+        int mn=(1e9+8),idx=0;
+        for(int i=0;i<n;i++){
+            if(mn>nums[i]){
+                mn=nums[i];
+                idx=i;
+            }
+        }
+        for(int i=0;i<n;i++)
+            if(i!=idx)
+                ans+=(nums[i]-mn);
         return ans;
     }
 };
