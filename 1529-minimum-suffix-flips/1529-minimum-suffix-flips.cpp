125217cc1e163f4dd4c70d5b2a1ea4c50dca6055
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-03 12:17:01 +0530

    Time: 24 ms (34.84%), Space: 9.4 MB (46.11%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //Continuous groups of 0s and 1s
+    //Continuous groups of 0s and 1s starting with leftmost 1
 int minFlips(string target) {
 
     int check=0,count=0;
