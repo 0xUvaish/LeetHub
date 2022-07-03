a16d3fa5bf78c1bd1489912b0ad1a10460e9aac0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-03 13:09:32 +0530

    Time: 7 ms (67.80%), Space: 7.3 MB (75.99%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int h = s.size() - 1;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] >= 65 && s[i] <= 90){
+                s[i] = s[i] - 'A' + 'a';
+            }
+        }
+        while(l < h){
+            while(!isalnum(s[l]) && l<h){
+                l++;
+            }
+            while(!isalnum(s[h]) && l<h){
+                h--;
+            }
+            if(s[l] != s[h])
+                return false;
+            l++; h--;
+        }
+        return true;
+    }
+};
