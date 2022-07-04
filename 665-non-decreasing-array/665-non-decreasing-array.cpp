8c51ecacd227734e4a4b2c9f5f5e01e4940b69c2
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-04 12:25:22 +0530

    Time: 49 ms (33.87%), Space: 27 MB (53.04%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -3,18 +3,19 @@ public:
     bool checkPossibility(vector<int>& nums) 
     {
         int cnt=0;
-        for(int i=0;i<nums.size()-1;i++)
+        int n = nums.size();
+        for(int i=0;i<n-1;i++)
         {
-            if(nums[i]>nums[i+1])   //violation condition
+            if(nums[i]>nums[i+1])   //violation condition - decreasing
             {
                 if(cnt==1) 
                     return false;
                 cnt++;
                 
                 if(i==0 || nums[i+1]>=nums[i-1])
-                    nums[i] = nums[i+1];  //first possiblity :-> if next element grater then or equal to previous element then update middle element to its next element
+                    nums[i] = nums[i+1];  //first possiblity :-> if next element greater than or equal to previous element then update middle element to its next element
                 else
-                    nums[i+1] = nums[i];  //else update next element to middle one
+                    nums[i+1] = nums[i];  //else update next element to middle one  - Diagram will clear it
             }
         }
         return true; 
