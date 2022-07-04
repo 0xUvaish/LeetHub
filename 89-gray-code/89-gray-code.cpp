4ce89e533eacadb54d18cbaacc0c8601c05fa8ac
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-04 15:55:14 +0530

    Time: 25 ms (23.86%), Space: 11.8 MB (80.01%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -3,11 +3,11 @@ public:
 vector<int> grayCode(int n) {
     
     vector<int>ans;
-    for(int i=0;i<=(pow(2,n)-1);i++)
-    {
+    int k = pow(2,n);
+    
+    for(int i=0;i<k;i++)
         ans.push_back(i ^ (i>>1));
-    }
     
     return ans;
-}
+    }
 };
