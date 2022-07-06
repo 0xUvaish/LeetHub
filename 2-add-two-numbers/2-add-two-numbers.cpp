cbda70bf586e754100e92545fd1e63f8fc2bb2af
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-06 09:16:33 +0530

    Time: 56 ms (47.39%), Space: 71.5 MB (11.52%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -16,7 +16,7 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
         ListNode* tmp = dummy;
         
         int carry = 0;
-        while(l1 || l2)
+        while(l1 || l2 || carry)
         {
             int first = (l1) ? l1->val : 0;
             int second = (l2) ? l2->val : 0;
@@ -33,9 +33,6 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
             
             tmp = tmp->next;
         }
-    
-        if(carry) 
-            tmp->next = new ListNode(carry);
     
         return dummy->next;
     }
