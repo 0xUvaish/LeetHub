56bb24dc57298b56b0f164c714247cf1b5a3cdc2
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-19 23:08:23 +0530

    Time: 15 ms (80.55%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,56 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    //Sol1: DP
+//     int lengthOfLIS(vector<int>& nums) {
         
-       int n = nums.size();
-       int lis[n];
-       int maxLIS = 1;
-       lis[0] = 1;
+//        int n = nums.size();
+//        int lis[n];
+//        int maxLIS = 1;
+//        lis[0] = 1;
         
-       for(int i=1;i<n;i++)
-       {
-           lis[i] = 1;
-           for(int j=0;j<i;j++)
-           {
-               if(nums[j]<nums[i])
-               {
-                   lis[i] = max(lis[i],lis[j]+1);
-                   maxLIS = max(maxLIS, lis[i]);
-               }
-           }
-       }    
+//        for(int i=1;i<n;i++)
+//        {
+//            lis[i] = 1;
+//            for(int j=0;j<i;j++)
+//            {
+//                if(nums[j]<nums[i])
+//                {
+//                    lis[i] = max(lis[i],lis[j]+1);
+//                    maxLIS = max(maxLIS, lis[i]);
+//                }
+//            }
+//        }    
 
-        return maxLIS;
+//         return maxLIS;
+//     }
+    
+    
+    //Sol2: Binary Search - Striver
+    int findCeil(vector<int> &tail, int x)
+    {
+        int k = tail.size();
+        int l = 0, r = k - 1;
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+            if (tail[m] >= x)
+                r = m;
+            else
+                l = m + 1;
+        }
+        return r;
+    }
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> tail(1, nums[0]);
+        int n = nums.size();
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] > tail.back())
+                tail.push_back(nums[i]);
+            else
+                tail[findCeil(tail, nums[i])] = nums[i];
+        }
+        return tail.size();
     }
 };
