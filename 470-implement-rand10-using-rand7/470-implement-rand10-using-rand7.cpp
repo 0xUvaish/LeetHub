00f0c11af643bffe2d15a6cb182976b27ed14410
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-25 22:46:41 +0530

    Time: 117 ms (17.02%), Space: 7.4 MB (62.18%) - LeetHub

--- 470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.cpp
+++ 470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.cpp
@@ -4,12 +4,28 @@
 
 class Solution {
 public:
-    int g=0;
+    //Without using rand7()
+//     int g=0;
+//     int rand10() {
+//         g++;
+//         if(g%10==0)
+//             return 10;
+        
+//         return g%10;
+//     }
     
-    int rand10() {
-        g++;
-        if(g%10==0)
-            return 10;
-        return g%10;
+    //Using rand7()
+    int tmp;
+    int rand10()
+    {
+        int k=rand7() + tmp;
+        if(k==10)
+            tmp = 0;
+        else 
+        {
+            k=k%10;
+            tmp=k;
+        }
+        return k;
     }
 };
