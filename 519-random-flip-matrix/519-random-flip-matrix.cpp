6b3682cd7096cfea9a0723af73b0484460df5fa3
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-25 22:09:05 +0530

    Time: 16 ms (89.84%), Space: 19 MB (61.50%) - LeetHub

---
+++ 519-random-flip-matrix/519-random-flip-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+  unordered_map<int,int>table;
+  int x, y;
+  
+  Solution(int m, int n)
+  {
+    y = m, x = n;      
+  }
+    
+  vector<int> flip()
+  {
+    int Y = rand() % y;
+    int X = rand() % x;
+    int id;
+    
+    while(table. count(id = (Y << 14) + X ))
+    {
+      if(++X == x) X = 0, Y++;
+        
+      if(Y == y) 
+          Y = 0;
+    } 
+    
+    table[id] = 1;
+    return {Y, X};    
+  }
+    
+  void reset() 
+  {
+    table.clear();   
+  }
+    
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
