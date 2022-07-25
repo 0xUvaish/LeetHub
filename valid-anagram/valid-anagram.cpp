eb8c06619fb427329deb7704d123c7eaf8423167
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-25 14:52:46 +0530

    Time: 15 ms, Memory: 7.4 MB - LeetHub

---
+++ valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    
+    if(s.size()!=t.size()) 
+        return 0;
+        
+    vector<int> a(26,0);
+    vector<int> b(26,0);
+        
+    for(int i=0;i<s.size();i++)
+    {
+       a[s[i]-'a']++;
+       b[t[i]-'a']++;
+    }
+      
+    return a==b;
+    }
+};
