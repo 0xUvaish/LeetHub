0832c0f0f3a961ea3ef3998268ab2161f969fc16
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-30 18:16:02 +0530

    Time: 50 ms (19.86%), Space: 11.2 MB (50.80%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -21,22 +21,43 @@ public:
 class Solution {
 public:
     //M1: Recursive
-vector<int> preorder(Node* root) 
-{
-    vector<int> ans;
-    preTraversal(root, ans);
-	return ans;
-}
+// vector<int> preorder(Node* root) 
+// {
+//     vector<int> ans;
+//     preTraversal(root, ans);
+// 	return ans;
+// }
     
-void preTraversal(Node* root, vector<int>& ans)
-{
-    if(!root) 
-        return;
+// void preTraversal(Node* root, vector<int>& ans)
+// {
+//     if(!root) 
+//         return;
     
-    ans.push_back(root -> val);
-    for (auto& child : root -> children)
-        preTraversal(child, ans);
-}
+//     ans.push_back(root -> val);
+//     for (auto& child : root -> children)
+//         preTraversal(child, ans);
+// }
     
  //M2: Iterative
+
+vector<int> preorder(Node* root)
+{
+    if(!root)
+        return {};
+    stack<Node*> st;
+    vector<int> ans;
+    st.push(root);
+    
+    while(!st.empty())
+    {
+        auto best = st.top();
+        ans.push_back(best->val);
+        st.pop();
+        
+        int n = best->children.size();
+        for(int i=n-1;i>=0;i--)
+            st.push(best->children[i]);
+    }
+return ans;
+}    
 };
