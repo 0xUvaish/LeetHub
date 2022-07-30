ead0247cbe29b0bc7b942abfc4fcc41d53299ae5
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-30 18:05:53 +0530

    Time: 32 ms (61.88%), Space: 11.1 MB (78.24%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    //M1: Recursive
+vector<int> preorder(Node* root) 
+{
+    vector<int> ans;
+    preTraversal(root, ans);
+	return ans;
+}
+    
+void preTraversal(Node* root, vector<int>& ans)
+{
+    if(!root) 
+        return;
+    
+    ans.push_back(root -> val);
+    for (auto& child : root -> children)
+        preTraversal(child, ans);
+}
+    
+ //M2: Iterative
+};
