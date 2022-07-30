3c9593292b943e43283aef4fb5ac0112c11a977b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-07-30 18:26:21 +0530

    Time: 131 ms (19.24%), Space: 21.8 MB (56.72%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    int r,c,ans=0,f;
-    void dfs(vector<vector<int>>& g,int i,int j,int &cnt)
-    {
-        if(i>=r || i<0 || j>=c || j<0 || g[i][j]!=1){
-            if(i>=r || i<0 || j>=c || j<0) f=1;
-            return;
-        }
-        g[i][j]=2;
-        cnt+=1;
-        dfs(g,i,j+1,cnt);
-        dfs(g,i,j-1,cnt);
-        dfs(g,i+1,j,cnt);
-        dfs(g,i-1,j,cnt);
-    }
-    
-    int numEnclaves(vector<vector<int>>& g) 
-    {
-        r=g.size(),c=g[0].size();
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++)
-                if(g[i][j]==1)
-                {
-                    f=0;
-                    int cnt=0;
-                    dfs(g,i,j,cnt);
-                    if(!f)ans+=cnt;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    void DFS(vector<vector<int>>& grid, int x,int y){
+        grid[x][y] = 0;
+        for(int i=0;i<4;i++){
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if(xx>=0 and xx<grid.size() and yy>=0 and yy<grid[0].size()){
+                if(grid[xx][yy] == 1){
+                    DFS(grid,xx,yy);
                 }
-        return ans;                    
+            }
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        // so we apply floodfill from the boundary of grid.
+        int rows = grid.size();
+        int cols = grid[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(i*j == 0 or i == rows-1 or j == cols-1){
+                    if(grid[i][j] == 1){
+                        DFS(grid,i,j);
+                    }
+                }
+            }
+        }
+        int countisland = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    countisland++;
+                }
+            }
+        }
+        return countisland;
     }
 };
