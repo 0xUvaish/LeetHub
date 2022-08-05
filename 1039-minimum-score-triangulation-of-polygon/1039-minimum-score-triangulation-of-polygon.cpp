0c6ce41b0498bf6e746118f7f429efa695be0647
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-05 22:53:09 +0530

    Time: 7 ms (89.19%), Space: 8.2 MB (82.09%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -4,17 +4,17 @@ int t[101][101];
 
 int solve(vector<int>& arr, int i, int j)
 {
-    if(i>=j)
+    if(i+1==j)
         return 0;
     
     if(t[i][j] != -1)
         return t[i][j];
     
     int mn= INT_MAX;
-    for(int k=i; k<j; k++)
+    for(int k=i+1; k<j; k++)
     {
-        int temp= solve(arr, i, k) + solve(arr, k+1, j) + 
-            arr[i-1]*arr[k]*arr[j];
+        int temp= solve(arr, i, k) + solve(arr, k, j) + 
+            arr[i]*arr[k]*arr[j];
         
         if(temp<mn)
         t[i][j]=mn= min(mn, temp);
@@ -28,7 +28,7 @@ int minScoreTriangulation(vector<int>& values)
 {
     
     memset(t,-1,sizeof(t));
-    int i=1; 
+    int i=0; 
     int j=values.size()-1;
     return solve(values, i, j);
 }
