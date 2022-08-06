cb27c05a953bc07f61bd8805738eb836f8bcd689
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-06 10:38:48 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (75.51%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -4,21 +4,21 @@ public:
         int mp1[10] = {0};
         int mp2[10] = {0};
         int bull=0,cow=0;
-        for(int i=0;i<secret.length();i++) {
-            if(secret[i]==guess[i]) {
+        
+        for(int i=0;i<secret.length();i++)
+        {
+            if(secret[i]==guess[i]) 
                 bull++;
-            } else {
+            else 
+            {
                 mp1[secret[i]-'0']++;
                 mp2[guess[i]-'0']++;
             }
         }
         
-        for(int i=0;i<10;i++) {
-            if(mp1[i]!=0 && mp2[i]!=0) {
+        for(int i=0;i<10;i++) 
+            if(mp1[i]!=0 && mp2[i]!=0) 
                 cow += min(mp1[i], mp2[i]);
-                
-            }
-        }
         
         return to_string(bull) + "A" + to_string(cow) + "B";
              
