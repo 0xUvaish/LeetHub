7c0bda1af0dfb7eeeae51c593d1a75ba58d5d405
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-07 10:56:00 +0530

    Time: 4 ms (23.15%), Space: 6 MB (27.59%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //M1: Brute Force
+    int maximumSwap(int num) 
+    {
+        string s=to_string(num);
+        int n=stoi(s);
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=i+1;j<s.size();j++)
+            {
+                swap(s[i],s[j]);
+                n=max(n,stoi(s));
+                swap(s[i],s[j]);
+            }
+        }
+        return n;
+    }
+};
