7aa6c38c467a411960ac76e40b0ee86caf00e3c0
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-09 00:28:19 +0530

    Time: 346 ms (79.95%), Space: 8 MB (75.19%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -4,19 +4,17 @@ public:
     bool visited[6][6];
     void solve(vector<vector<char>>& board, string& word,int i,int j,int idx)
     {
-if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || visited[i][j] == true || result == true)
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || visited[i][j] == true || result == true || board[i][j] != word[idx])
            return;
+        
+        if(idx == word.size()-1)
+            result = true;
+        
         visited[i][j] = true;
-        if(board[i][j] == word[idx])
-        {
-            if(idx == word.size()-1)
-                result = true;
-            
-            solve(board,word,i+1,j,idx+1);
-            solve(board,word,i-1,j,idx+1);
-            solve(board,word,i,j+1,idx+1);
-            solve(board,word,i,j-1,idx+1);
-        }
+        solve(board,word,i+1,j,idx+1);
+        solve(board,word,i-1,j,idx+1);
+        solve(board,word,i,j+1,idx+1);
+        solve(board,word,i,j-1,idx+1);
         visited[i][j] = false;
     }
     
