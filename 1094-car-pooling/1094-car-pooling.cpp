a160d7b32099fcffbd92a339d7365f1e0320d4dc
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-10 01:13:45 +0530

    Time: 48 ms (15.86%), Space: 18.2 MB (5.66%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -9,22 +9,22 @@ public:
     {
         sort(trips.begin(),trips.end(),compare);
         int curr_pass=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;  //When to drop and whom
         
         for(int i=0;i<trips.size();i++)
         {
-            while(!pq.empty()&&pq.top().first<=trips[i][1])
+            while(!pq.empty()&&pq.top().first<=trips[i][1])  //Drop till current time
             {
                 curr_pass-=trips[pq.top().second][0];
                 pq.pop();
             }
             
-            curr_pass+=trips[i][0];
+            curr_pass+=trips[i][0];                          //Pickup current
             
             if(curr_pass>capacity)
                 return false;
             
-            pq.push({trips[i][2],i});
+            pq.push({trips[i][2],i});                        //Next drop for current   
         }
         
         return true;
