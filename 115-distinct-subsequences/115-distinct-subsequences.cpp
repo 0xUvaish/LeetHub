cb9315d69bf7d846246b6bfde44d1584196c5840
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-11 00:11:39 +0530

    Time: 47 ms (70.77%), Space: 10.7 MB (77.63%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-        int dp[1001][1001];
+    //Extension of 2 pointer easy problem
+    
+    int dp[1001][1001];
     int solve(string &s, string &t, int i, int j)
     {
-        if (j == t.length())
+        if(j==t.length())
             return 1;
         
-        if (i == s.length())
+        if(i==s.length())
             return 0;
-        else if (dp[i][j] != -1)
+        
+        if (dp[i][j] != -1)
             return dp[i][j];
-        else if (s[i] == t[j])
+        
+        if(s[i] == t[j])
             return dp[i][j] = solve(s, t, i + 1, j + 1) + solve(s, t, i + 1, j);
         else
             return dp[i][j] = solve(s, t, i + 1, j);
