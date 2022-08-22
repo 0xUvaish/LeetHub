95e403b43be58c01da7fae026b7aaf3d8428ddf6
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-22 13:35:51 +0530

    Time: 16 ms (94.46%), Space: 6 MB (12.98%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
+    //M1: Loop Method log3n
+//     bool isPowerOfThree(int n) {
+//       if(n==0)
+//         return false;
+    
+//     while(n)
+//     {
+//         if(n==1)
+//             return true;
+//         if(n<1)
+//             return false;
+//         if(n%3!=0)
+//             return false;
+//         n=n/3;
+//     }
+    
+//     return true;     
+//     }
+    
+    
+    //M2: Without loops/recursion - logn still
     bool isPowerOfThree(int n) {
-      if(n==0)
-        return false;
-    
-    while(n)
-    {
-        if(n==1)
-            return true;
-        if(n<1)
+        if(n==0) 
             return false;
-        if(n%3!=0)
-            return false;
-        n=n/3;
-    }
-    
-    return true;     
+        
+        float x=log(n)/log(3);
+        return pow(3,x)==n;
     }
 };
