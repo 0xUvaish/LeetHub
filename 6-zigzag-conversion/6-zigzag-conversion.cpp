5638497360bb2ac3b7ffb65f7a48b8688cae6aa1
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-24 17:03:26 +0530

    Time: 8 ms (89.34%), Space: 10.8 MB (27.29%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> vec(numRows);
+        
+        int ind=0;
+        int n = s.size();
+        int i;
+        while(ind<n)
+        {
+            i=0;
+            while(i<numRows && ind<n)
+            {
+                vec[i++] += s[ind++];
+            }
+            
+            i=numRows-2;
+            while(i>=1 && ind<n)
+            {
+                vec[i--] += s[ind++];
+            }
+        }
+        
+        string ans="";
+        for(auto x: vec)
+            for(auto y: x)
+                ans += y;
+        
+        return ans;
+    }
+};
