960316780c581a3e1f9fa09236692fd6bec2de4b
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-28 10:01:19 +0530

    Time: 7 ms (98.13%), Space: 9.3 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -2,19 +2,51 @@ class Solution {
 public:
     
     //Sol 1: Insertion Sort O(n^3) Sol
-    vector<vector<int>> diagonalSort(vector<vector<int>> &mat) {
+//     vector<vector<int>> diagonalSort(vector<vector<int>> &mat) {
 
-    for(int t=1;t<mat.size();t++)
+//     for(int t=1;t<mat.size();t++)
+//     {
+//         for(int i=1;i<mat.size();i++)
+//         {
+//             for(int j=1;j<mat[0].size();j++)
+//             {
+//                 if(mat[i][j]<mat[i-1][j-1])
+//                     swap(mat[i][j],mat[i-1][j-1]);
+//             }
+//         }
+//     }
+//     return mat;
+//     }
+    
+    
+    //Sol2: Priority Queue
+    void diagonalSorter(int i,int j,vector<vector<int>>&mat)
     {
-        for(int i=1;i<mat.size();i++)
+        int m=mat.size(),n=mat[0].size();
+        priority_queue<int>arr;
+        
+        while(i<m && j<n)
         {
-            for(int j=1;j<mat[0].size();j++)
-            {
-                if(mat[i][j]<mat[i-1][j-1])
-                    swap(mat[i][j],mat[i-1][j-1]);
-            }
+            arr.push(mat[i][j]);
+            i++,j++;
+        }
+        i--,j--;
+		
+        while(i>=0 && j>=0)
+        {
+            mat[i][j]=arr.top();
+            arr.pop();
+            i--,j--;
         }
     }
-    return mat;
+    
+    vector<vector<int>> diagonalSort(vector<vector<int>> mat)
+    {
+        int m=mat.size(),n=mat[0].size();
+        
+        for(int i=m-1,j=0; i>=0 && j<n; i==0?j++:i--)
+            diagonalSorter(i,j,mat);
+        
+        return mat;
     }
 };
