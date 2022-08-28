b5f92d1448d17204ea65c5fbbb58d5b88c456537
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-08-28 09:52:24 +0530

    Time: 78 ms (5.15%), Space: 8.7 MB (95.66%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
+    
+    //Sol 1: Insertion Sort O(n^3) Sol
     vector<vector<int>> diagonalSort(vector<vector<int>> &mat) {
 
-    for(int t=0;t<mat.size();t++)
+    for(int t=1;t<mat.size();t++)
     {
         for(int i=1;i<mat.size();i++)
         {
