07debffadd517bbd0e8bfb49d9350d0f5a7c245e
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-09-10 09:29:12 +0530

    Time: 30 ms (29.62%), Space: 11.9 MB (53.52%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,13 +2,25 @@ class Solution {
 public:
     int dp[1001][101][2];
     int solve(int k, vector<int>& prices, int pos, bool hold){
-        if(pos>=prices.size()) return 0;
-        if(dp[pos][k][hold]!=-1) return dp[pos][k][hold];
+       
+        if(pos>=prices.size()) 
+            return 0;
+        
+        if(dp[pos][k][hold]!=-1) 
+            return dp[pos][k][hold];
+        
         int sell=0, notsell=0, buy=0, notbuy=0;
-        if(!hold and k) buy = solve(k-1,prices,pos+1,!hold) - prices[pos];
+        
+        if(!hold and k) 
+            buy = solve(k-1,prices,pos+1,!hold) - prices[pos];
+        
         notbuy = solve(k,prices,pos+1,hold);
-        if(hold) sell = solve(k,prices,pos+1,!hold) + prices[pos];
+        
+        if(hold) 
+            sell = solve(k,prices,pos+1,!hold) + prices[pos];
+        
         notsell = solve(k,prices,pos+1,hold);
+        
         return dp[pos][k][hold] = max({buy,sell,notbuy,notsell});
     }
     
