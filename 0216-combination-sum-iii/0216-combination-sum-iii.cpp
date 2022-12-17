72dc8545d12dc35d6af553732639229e69ff0d54
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-12-17 15:09:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (84.08%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>>res;
+    void helper(vector<int>& candidates, int k, int target, vector<int>&ans, int ind)
+    {
+        int N = ans.size();
+        if(N>k)
+            return;
+
+        if(target==0 && N==k)
+        {
+            res.push_back(ans);
+            return;
+        }
+        
+        for(int i=ind; i<candidates.size(); i++)
+        {
+            if(candidates[i]>target)                        //As next nos. will also be greater
+                break;
+
+            ans.push_back(candidates[i]);
+            helper(candidates, k, target-candidates[i], ans, i+1);  //Can be used atmost once
+            ans.pop_back();
+
+        }
+
+    }
+    
+    vector<vector<int>> combinationSum3(int k , int n) {
+        
+        vector<int>temp;
+        vector<int>list = {1,2,3,4,5,6,7,8,9};
+
+        helper(list, k, n, temp, 0);
+        
+        return res;
+    }
+};
