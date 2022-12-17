f8289c61e791ee77cc9c87acd3273b413eb6b9ee
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-12-17 15:36:26 +0530

    Time: 5 ms (39.27%), Space: 6.1 MB (7.28%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+// Right Shift Method
+// int hammingWeight(uint32_t n) 
+// {
+//         int count = 0;
+//         while(n!=0) 
+//         {
+//             if(n&1)
+//                 count++;
+            
+//             n = n>>1;
+//         }
+    
+//         return count;
+//     }
+    
+
+// n&(n-1) Method - O(No. of set bits only) - Optimised
+    
+int hammingWeight(uint32_t n) 
+{
+        int count = 0;
+        while(n!=0) 
+        {
+            count++;            
+            n = (n & (n-1));
+        }
+    
+    return count;
+    
+    }
+};
