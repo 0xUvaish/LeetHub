2b8d7563f4405e7fbaed4504034e3164879ab553
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-12-17 19:56:15 +0530

    Time: 5 ms (43.50%), Space: 6.6 MB (51.65%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+        int lengthOfLastWord(string s) {
+
+            if(s.length()==1)
+                return 1;
+
+            int j = s.length()-1;
+
+            while(s[j] == ' ')
+                j--;
+
+            int counter = 0;
+    
+            while(j>=0 && s[j] != ' ')
+            {
+                counter++;
+                j--;
+            }
+            
+            return counter;
+        }
+};
