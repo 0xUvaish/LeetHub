210ee83a19d7ebddb6b1cfccee84ddf0a7855023
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2022-12-19 14:50:14 +0530

    Time: 213 ms (66.37%), Space: 42.5 MB (75.45%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    int dfs(int cur, vector<vector<int>> &graph, vector<int> &quiet, vector<int> &answer) {
+        
+        int leastQuietPerson = cur;
+        for(int &richer : graph[cur])
+        {
+            // Memoization
+            int ret = (answer[richer] != -1) ? answer[richer] : dfs(richer, graph, quiet, answer);
+            
+            if (quiet[ret] < quiet[leastQuietPerson]) 
+                leastQuietPerson = ret;
+        }
+        
+        return answer[cur] = leastQuietPerson;
+    }
+    
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+       
+        int n = quiet.size();
+        vector<vector<int>> graph(n);
+        
+        for (vector<int> &r : richer)
+            graph[r[1]].push_back(r[0]);
+        
+        vector<int> answer(n, -1);
+        
+        for (int cur = 0; cur < n; cur++) 
+            dfs(cur, graph, quiet, answer);
+        
+        return answer;
+    }
+};
