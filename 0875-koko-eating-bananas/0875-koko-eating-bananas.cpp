c1936fbc913ba2014fb6d77afd72117afb8a3aef
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2023-03-08 09:40:47 +0530

    Time: 42 ms (88.59%), Space: 19 MB (11.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+	bool is_satisfying(vector<int>& piles,int k,int h)
+    {
+		long long int s=0;
+		for(int v:piles) 
+            s+=ceil((v*1.0)/k);
+        
+		return s<=h;
+	}
+    
+	int minEatingSpeed(vector<int>& piles, int h) 
+    {
+		int l=1,r=0,k=0,res=0;
+        
+		for(int v:piles) 
+            r=max(r,v);
+        
+		while(l<=r)
+        {
+			k=l+(r-l)/2;
+            
+			if(is_satisfying(piles,k,h))
+            {
+				res=k;
+				r=k-1;
+			}
+			else l=k+1;
+		}
+        
+	return res;
+	}
+};
