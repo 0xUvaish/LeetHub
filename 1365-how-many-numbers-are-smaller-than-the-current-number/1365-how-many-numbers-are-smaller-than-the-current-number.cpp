82217f158cdd403c6ffb4c52efdc7d83ab58ecc8
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-08-11 12:19:44 +0530

    Time: 6 ms (80.42%), Space: 14.5 MB (5.43%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n,0); 
+    //Brute force
+//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n,0); 
         
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-               if(j!=i && nums[j]<nums[i])
-                   ans[i]++;
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                if(j!=i && nums[j]<nums[i])
+//                    ans[i]++;
+//             }
+//         }
+        
+//     return ans;
+//     }
+    
+    
+    
+        vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted_nums(nums.begin(), nums.end());
+        sort(sorted_nums.begin(), sorted_nums.end());
+        
+        unordered_map<int, int> count;
+        for (int i = 0; i < n; i++) {
+            if (count.find(sorted_nums[i]) == count.end()) {
+                count[sorted_nums[i]] = i;
             }
         }
         
-    return ans;
+        vector<int> ans;
+        for (int num : nums) {
+            ans.push_back(count[num]);
+        }
+        
+        return ans;
     }
 };
