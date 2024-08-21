30207fa54419d28ebd2e989fb56765424b538f34
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-08-21 20:34:22 +0530

    Time: 9 ms (9.09%), Space: 14.9 MB (29.53%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+    
+        ListNode *temp1=new ListNode(0);
+        ListNode *temp2=new ListNode(0);
+         
+        ListNode *t1=temp1;
+        ListNode *t2=temp2;
+        
+        ListNode *curr=head;
+        while(curr)
+        {
+            if(curr->val<x)
+            {
+                temp1->next=curr;
+                temp1=temp1->next;
+            }
+            else
+            {
+                temp2->next=curr;
+                temp2=temp2->next;
+            }
+            curr=curr->next;
+        }
+        
+        if(temp2->next)
+        temp2->next=NULL; //Draw diagram to understand it - Detach extra links
+        
+        if(!t1->next)
+        return t2->next;
+
+        temp1->next=t2->next;
+        return t1->next;
+        
+    
+    }
+};
