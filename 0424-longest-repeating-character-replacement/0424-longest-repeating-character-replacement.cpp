a8161ce6ba3b7ea11f37bb455832f0fe8f10e78b
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-08-28 20:27:37 +0530

    Time: 3 ms (96.20%), Space: 8.3 MB (82.59%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    //Method 1: O(26*n) Calculate max freq every time without taking extra variable
+    
+    //Method 2: O(n) TC
+    int characterReplacement(string s, int k) {
+        
+        int i=0;
+        int maxFreq=0;
+        vector<int>v(26,0);
+        
+        int ans=INT_MIN;
+        
+       for(int j=0;j<s.size();j++)
+        {
+            v[s[j]-'A']++;
+            maxFreq = max(maxFreq, v[s[j]-'A']);
+           
+            while(j-i+1-maxFreq>k)
+            {
+                v[s[i]-'A']--;
+                i++;
+            }
+           
+            ans=max(ans,j-i+1);
+        }
+        
+        return ans;
+    }
+};
