f47e387136abefec7a94f0c242a9a584d516cac7
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-09-14 14:14:38 +0530

    Time: 4 ms (34.09%), Space: 9.3 MB (61.76%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+vector<string> ans;
+
+void solve(string &s, set<string> &dict, int index, string asf)
+{
+    if(index==s.length())
+    {
+        asf = asf.substr(0, asf.size()-1);
+        ans.push_back(asf);
+        return;
+    }
+    
+    string temp="";
+    for(int i=index; i<s.length();i++)
+    {
+        temp += s[i];
+        if(dict.find(temp)!=dict.end())
+        {
+            solve(s, dict, i+1, asf+temp+" ");
+        }
+    }
+}
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+    
+    set<string> dict;
+    for(auto x: wordDict)
+        dict.insert(x);
+    
+    solve(s, dict, 0, "");
+    return ans; 
+    }
+    
+
+};
