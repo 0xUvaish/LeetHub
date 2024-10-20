788bfed09cb8d629b960e7c7dc3bc215d3e41da3
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-10-20 11:22:09 +0530

    Time: 0 ms (100.00%), Space: 16.8 MB (26.45%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+ //Follow up In case all are equal with element not found then it will end up O(n) TC
+    
+    bool search(vector<int>& nums, int X) 
+    {
+        int n = nums.size(), l = 0, r = n - 1;
+        
+        while(l <= r)
+        {
+            int m = l + (r-l)/2;
+            
+            if(nums[m] == X) 
+                return true;
+            
+            else if(nums[l] == nums[m] && nums[r] == nums[m]) //Additional case for duplicates
+                l++, r--;                                      
+            
+            else if(nums[l] <= nums[m])
+            {
+                
+                if(nums[l] <= X && nums[m] > X)
+                    r = m - 1;
+                else 
+                    l = m + 1;
+            }
+            else 
+            {
+                if(nums[r] >= X && nums[m] < X) 
+                    l = m + 1;
+                else 
+                    r = m - 1;
+            }
+            
+        }
+        return false;
+    }
+
+};
