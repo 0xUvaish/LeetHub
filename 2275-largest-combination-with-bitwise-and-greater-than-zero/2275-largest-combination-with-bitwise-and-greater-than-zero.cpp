8900aecd053db049cfa349d625a37ad95b2a7d9d
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-11-07 20:00:21 +0530

    Time: 11 ms (95.52%), Space: 60.2 MB (32.98%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+class Solution {
+public:
+    int largestCombination(std::vector<int>& nums) {
+        int bits[32] = {0};
+        for (int num : nums) {
+            int j = 0;
+            while (num > 0) {
+                bits[j++] += num & 1;
+                num >>= 1;
+            }
+        }
+        return *std::max_element(bits, bits + 32);
+    }
+};
