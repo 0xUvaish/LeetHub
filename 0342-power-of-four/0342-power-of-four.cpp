717fe691e64643722aaaf9b73a83341825f3090c
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-11-10 10:39:30 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (70.67%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -14,11 +14,14 @@ public:
     
 //Sol2: Bit Manipulation
     bool isPowerOfFour(int n) {
-        return n>0 && (n&(n-1))==0 && (n&0x55555555);
+        //return n>0 && (n&(n-1))==0 && (n&0x55555555);
 		// n>0, n should be positive
 		// n&(n-1) ensure it is power of 2
 		// 5 is "0101" so n&0x55555... ensure that set bit is at the even position.
 		// so if n is power of 2 and set bit is at even position -> n is power of 4
+        
+        
+        return n>0 && (n&(n-1))==0 && !(n&0xAAAAAAAA); //negate mask approach
     }
 
 };
