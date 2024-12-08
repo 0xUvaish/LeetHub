f03102061867611e9058a57e1b15263a585ac2c9
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-12-08 21:36:29 +0530

    Time: 16 ms (98.61%), Space: 73.9 MB (93.21%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -10,21 +10,21 @@ public:
         {
             int curr = n[j];    
             
-            while(!maxx.empty() && n[maxx.back()]<=curr)
+            while(!maxx.empty() && n[maxx.back()]<=curr)     //2nd step of SW Maximum
                 maxx.pop_back(); 
             
-            maxx.push_back(j);
+            maxx.push_back(j);                               //3rd step of SW Maximum
             
-            while(!minn.empty() && n[minn.back()]>=curr)
+            while(!minn.empty() && n[minn.back()]>=curr)     //2nd step of SW Minimum
                 minn.pop_back(); 
             
-            minn.push_back(j);
+            minn.push_back(j);                               ////3rd step of SW Minimum
             
             
             
             while(abs(n[maxx.front()]-n[minn.front()]) > l)
             {
-                if(maxx.front()==i)
+                if(maxx.front()==i)                         
                     maxx.pop_front();
                 
                 if(minn.front()==i)
