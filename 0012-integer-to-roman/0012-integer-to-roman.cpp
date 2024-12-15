11352026921f90ecff56f6afd8363a02b5cb427f
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-12-15 11:11:56 +0530

    Time: 12 ms (12.40%), Space: 22.6 MB (5.32%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        const vector<vector<string>> intToRoman = 
+        {
+            {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},  //1-digit
+            {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},  //2-digits
+            {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},  //3-digits
+            {"", "M", "MM", "MMM", "MMMM"}                                 //4-digits
+        };
+        
+        string result="";
+        int row=0, col;
+        
+        while(num>0)
+        {
+            col = num%10;
+            num /= 10;
+            
+            result = intToRoman[row++][col] + result;
+        }
+        
+        return result;
+    }
+};
