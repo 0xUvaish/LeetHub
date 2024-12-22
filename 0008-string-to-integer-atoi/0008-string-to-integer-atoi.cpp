bbfc2f731569395abd29c53aea09bf3a77f7073e
Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2024-12-22 10:15:17 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (73.20%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    //Method 1: stringstream object
+ // int myAtoi(string s) 
+ //   {
+ //       stringstream abc(s);
+ //        int x=0;
+ //        abc>>x;
+ //     return x;
+ //    }
+    
+    //Method2: O(n) TC, O(1) SC
+int myAtoi(string s)
+{
+    
+    int leadSpace= 0;
+    for(int i=0; i<s.length(); i++)
+        if(s[i]==' ')
+            leadSpace++;
+        else
+            break;
+    
+    s.erase(0, leadSpace);
+    
+    if(s.length()== 0)
+        return 0;
+    
+    int sign= 1;
+    int startIndex= 0;
+    
+    if(s[0]== '-'){
+        sign= -1;
+        startIndex= 1;
+    }
+    else if(s[0]== '+'){
+        sign= 1;
+        startIndex= 1;
+    }
+    
+
+    long long res= 0;
+    
+    for(int i= startIndex; i<s.length(); i++)
+    {
+        if(!(s[i]>= '0' && s[i]<= '9'))
+            return res* sign;
+        
+        res= res*10 + int(s[i]- '0');
+        
+        //Range is given
+        if(res*sign <= INT_MIN)
+            return INT_MIN;
+        else if(res*sign >= INT_MAX)
+            return INT_MAX;
+    }
+    
+    return res*sign;
+    }
+};
